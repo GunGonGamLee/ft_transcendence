8588c4d064566133fc618f654c3902b839399cbe
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-03 15:02:40 +0900

    feat(pingpong): CustomGame 모델 정의

--- pingpong/models.py
+++ pingpong/models.py
@@ -12,5 +12,12 @@ class CustomRoom(models.Model):
 
 class CustomGame(models.Model):
     room_id = models.ForeignKey(CustomRoom, on_delete=models.PROTECT, related_name='room')
+    player_1_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='player_1')
+    player_2_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='player_2')
+    player_1_score = models.PositiveSmallIntegerField(default=0)
+    player_2_score = models.PositiveSmallIntegerField(default=0)
+    who_is_winner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='winner')
     created_date = models.DateTimeField(auto_now_add=True)  # 생성 날짜
     updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
+
+
