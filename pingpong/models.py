29fa5e0fa5a0edb0a43c45911ebf47600fce9f3d
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-03 15:03:56 +0900

    feat(pingpong): Tournament 모델 추가

--- pingpong/models.py
+++ pingpong/models.py
@@ -21,3 +21,10 @@ class CustomGame(models.Model):
     updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
 
 
+class Tournament(models.Model):
+    player_1_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='player_1')
+    player_2_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='player_2')
+    player_3_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='player_3')
+    player_4_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='player_4')
+    created_date = models.DateTimeField(auto_now_add=True)  # 생성 날짜
+    updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
