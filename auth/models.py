be353e7286bad8144d480cd729f2d9c2a8191d7f
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-03 14:50:32 +0900

    style: 코드 포맷팅

--- auth/models.py
+++ auth/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
 
+
 class User(AbstractUser):
     """
         AbstractUser가 제공하는 필드들을 포함한다.
@@ -11,8 +12,8 @@ class User(AbstractUser):
     nickname = models.CharField(max_length=8, unique=True)
     rating = models.PositiveIntegerField(default=0)
     profile_image = models.ImageField(upload_to='profile_image/', null=True, blank=True)
-    created_date = models.DateTimeField(auto_now_add=True) # 생성 날짜
-    updated_date = models.DateTimeField(auto_now=True) # 수정 날짜
+    created_date = models.DateTimeField(auto_now_add=True)  # 생성 날짜
+    updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
 
 
 class FriendList(models.Model):
@@ -22,7 +23,6 @@ class FriendList(models.Model):
 
         class Meta:
             unique_together = ('user', 'friend')
-        created_date = models.DateTimeField(auto_now_add=True) # 생성 날짜
-        updated_date = models.DateTimeField(auto_now=True) # 수정 날짜
 
-        
+        created_date = models.DateTimeField(auto_now_add=True)  # 생성 날짜
+        updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
--- pingpong/models.py
+++ pingpong/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.conf import settings
 
+
 class CustomRoom(models.Model):
-	manager_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='manager')
-	room_description = models.CharField(max_length=42)
-	room_topic = models.CharField(max_length=10)
-	created_date = models.DateTimeField(auto_now_add=True) # 생성 날짜
-	updated_date = models.DateTimeField(auto_now=True) # 수정 날짜
-	
+    manager_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='manager')
+    room_description = models.CharField(max_length=42)
+    room_topic = models.CharField(max_length=10)
+    created_date = models.DateTimeField(auto_now_add=True)  # 생성 날짜
+    updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
+
 
 class CustomGame(models.Model):
-	room_id = models.ForeignKey(CustomRoom, on_delete=models.PROTECT, related_name='room')
-	created_date = models.DateTimeField(auto_now_add=True) # 생성 날짜
-	updated_date = models.DateTimeField(auto_now=True) # 수정 날짜
-
+    room_id = models.ForeignKey(CustomRoom, on_delete=models.PROTECT, related_name='room')
+    created_date = models.DateTimeField(auto_now_add=True)  # 생성 날짜
+    updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
