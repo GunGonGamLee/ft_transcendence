652eea5082833725e2be201c31087372b1ef1a28
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-11 14:23:55 +0900

    feat(login): 42 로그인 api 명세 작성

--- backend/login/views.py
+++ backend/login/views.py
@@ -2,35 +2,28 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
-from .serializers import GoogleLoginSerializer, FTLoginSerializer
+from .schema.responses import ft_login_response_schema
 
 
 class GoogleLoginView(APIView):
     @swagger_auto_schema(
         tags=["login"],
-        responses={302: GoogleLoginSerializer()},
     )
     def get(self, request):
-        response = validate_request(request)
-        serializer = GoogleLoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.data)
+
+        # serializer = GoogleLoginSerializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        return Response('')
 
 
 class FTLoginView(APIView):
     @swagger_auto_schema(
         tags=["login"],
-        responses={
-            e0
-        },
+        operation_id="42_login",
+        operation_summary="42 OAuth 로그인",
+        operation_description="42 OAuth 로그인 후 '/login/authorization'으로 리다이렉트",
+        responses=ft_login_response_schema,
     )
     def get(self, request):
-        request_data = {
-            'redirect_uri': f'https://{request.get_host()}/login/authorization'
-        }
-        serializer = FTLoginSerializer(data=request_data)
-        serializer.is_valid(raise_exception=True)
-        redirect_uri = serializer.validated_data['redirect_uri']
-        response = Response(status=302)
-        response['Location'] = redirect_uri
-        return response
+        return Response('')
+
