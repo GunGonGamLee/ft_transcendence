3be6d4e16dced96068b7501941b01e180f6c23f1
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-11 13:22:32 +0900

    refactor(serializer): class 이름 변경

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import GoogleLoginView, Login42View
+from .views import GoogleLoginView, FTLoginView
 
 
 urlpatterns = [
     path('google/', GoogleLoginView.as_view(), name="google_login"),
-    path('42/', Login42View.as_view(), name="login_42"),
+    path('42/', FTLoginView.as_view(), name="login_42"),
 ]
