10a07026e65539a93358cde8ddfa4fd07600c85f
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-15 11:38:14 +0900

    feat(users): users 모델 수정
    - 암시적으로 정의된 컬럼을 명시적으로 정의
    - profile_image에서 avatar로 변경
    - joined_date 컬럼 추가

--- backend/users/models.py
+++ backend/users/models.py
@@ -9,18 +9,21 @@ class User(AbstractUser):
         ID, PASSWORD, last_login, is_superuser, username, first_name,
         last_name, email, is_staff, is_active, date_joined
     """
-    nickname = models.CharField(max_length=8, unique=True)
-    rating = models.PositiveIntegerField(default=0)
-    profile_image = models.ImageField(upload_to='profile_image/', null=True, blank=True)
+    nickname = models.CharField(max_length=8, unique=True)  # 닉네임
+    email = models.EmailField(unique=True)  # 이메일
+    rating = models.PositiveIntegerField(default=0)  # 레이팅
+    avatar = models.ImageField(upload_to='avatar/', null=True, blank=True)  # 아바타 이미지
+    is_active = models.BooleanField(default=True)  # 활성화 여부
+    joined_date = models.DateTimeField(auto_now_add=True)  # 가입 날짜
     created_date = models.DateTimeField(auto_now_add=True)  # 생성 날짜
     updated_date = models.DateTimeField(auto_now=True)  # 수정 날짜
 
 
 class FriendList(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
-                             related_name='friend_list_as_user')
+        related_name='friend_list_as_user')
     friend = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
-                               related_name='friend_list_as_friend')
+        related_name='friend_list_as_friend')
 
     class Meta:
         unique_together = ('user', 'friend')
