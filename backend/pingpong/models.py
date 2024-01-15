44c2f20c27a5cc54e8201351b61f7f13d70a8bdc
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-15 12:02:01 +0900

    feat(pingpong): TournamentGame 모델 정의

--- backend/pingpong/models.py
+++ backend/pingpong/models.py
@@ -23,3 +23,15 @@ class Tournament(models.Model):
     class Meta:
         unique_together = ('player1_id', 'player2_id', 'player3_id', 'player4_id')
 
+
+class TournamentGame(models.Model):
+    tournament_id = models.ForeignKey('Tournament', on_delete=models.PROTECT, related_name='tournament')
+    player1_id = models.ForeignKey('Tournament', on_delete=models.PROTECT, related_name='tournament_player1')
+    player2_id = models.ForeignKey('Tournament', on_delete=models.PROTECT, related_name='tournament_player2')
+    winner_id = models.ForeignKey('Tournament', on_delete=models.PROTECT, related_name='tournament_winner')
+    player1_score = models.IntegerField(default=0)
+    player2_score = models.IntegerField(default=0)
+    match_index = models.IntegerField(default=0)
+    game_time = models.DurationField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
