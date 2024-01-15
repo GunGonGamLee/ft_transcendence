6722b16e9d185e15a0919d9677e00c5201b7dd31
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-15 12:01:15 +0900

    feat(pingpong): TwoPlayerGame 모델 정의

--- backend/pingpong/models.py
+++ backend/pingpong/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class TwoPlayerGame(models.Model):
+    winner_id = models.ForeignKey('users.User', on_delete=models.PROTECT, related_name='winner')
+    player1_id = models.ForeignKey('users.User', on_delete=models.PROTECT, related_name='player1')
+    player2_id = models.ForeignKey('users.User', on_delete=models.PROTECT, related_name='player2')
+    player1_score = models.IntegerField(default=0)
+    player2_score = models.IntegerField(default=0)
+    game_time = models.DurationField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
