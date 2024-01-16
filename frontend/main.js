8a20067da92eec3820cc58e0ada8b6b6945b866c
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-16 11:37:40 +0900

    feat(main): header를 렌더링하게 함

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,8 @@
 import App from "./src/app.js";
+import Header from "./src/common/header.js";
 import { $ } from "./src/utils/querySelector.js";
 
 window.addEventListener("DOMContentLoaded", (e) => {
+  new Header($("#header"));
   new App($("#app"));
 });
