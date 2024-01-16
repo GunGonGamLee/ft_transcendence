5d07ef6ec48d0f24d255c8a98e87c8d1e3c4435a
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-16 11:37:20 +0900

    chore(common): header js 파일 이동

---
+++ frontend/src/common/header.js
@@ -0,0 +1,29 @@
+import Router from "../router.js";
+import HistoriesHeader from "../pages/users/histories/historiesHeader.js";
+
+/**
+ * header 컴포넌트
+ * @param {HTMLElement} $container
+ */
+export default function header($container) {
+    this.$container = $container;
+
+    const init = () => {
+        new Router($container);
+    };
+
+    const render = () => {
+        this.$container.innerHTML = `
+        `;
+    }
+
+    init();
+    switch (location.pathname) {
+        case "/users/histories":
+            new HistoriesHeader($container);
+            break;
+        default:
+            render();
+            break;
+    }
+}
