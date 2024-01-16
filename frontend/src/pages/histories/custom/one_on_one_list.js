058086a5135f11ab151ba167879b1ac9d91cce10
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-16 16:49:10 +0900

    feat(header): histories의 header는 동적으로 css 파일을 가져와서 head에 link

--- frontend/src/pages/histories/custom/one_on_one_list.js
+++ frontend/src/pages/histories/custom/one_on_one_list.js
@@ -6,7 +6,16 @@ export default function oneOnOne($container) {
     }
 
     this.render = () => {
-
+        if (document.getElementsByTagName("head") !== null) {
+            document.getElementsByTagName("head")[0].insertAdjacentHTML(
+                "beforeend",
+                '<link rel="stylesheet" href="../../../../assets/css/histories/one_on_one.css"/>'
+            );
+        }
+        this.$container.innerHTML = `
+        <div class="histories" id="content-wrapper">d
+        </div>
+        `;
     }
 
     this.render();
