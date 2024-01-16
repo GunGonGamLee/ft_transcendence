dd2e5bcd462489dea8fe0b673c2f08c623a6c1d5
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-16 11:12:24 +0900

    feat(users): 전적 페이지 헤더 함수 프로토타입 구현

---
+++ frontend/src/pages/users/histories/historiesHeader.js
@@ -0,0 +1,18 @@
+/**
+ * 사용자 전적 페이지에 사용하는 header 컴포넌트
+ * @param {HTMLElement} $container
+ */
+export default function HistoriesHeader($container) {
+    this.$container = $container;
+
+    this.setState = () => {
+        this.render();
+    }
+
+    this.render = () => {
+        this.$container.innerHTML = `
+        `
+    }
+
+    this.render();
+}
