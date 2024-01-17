09cbba633030fb36099557fa39a05d7354eb3a38
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-17 17:34:49 +0900

    feat(histories): 사용자 전적 페이지 기본 레이아웃 작성

--- frontend/src/pages/histories/pages.js
+++ frontend/src/pages/histories/pages.js
@@ -10,10 +10,30 @@ export default function pages($container) {
             document.getElementsByTagName("head")[0].insertAdjacentHTML(
                 "beforeend",
                 '<link rel="stylesheet" href="../../../assets/css/histories.css"/>'
+
             );
         }
         this.$container.innerHTML = `
-        <div class="histories" id="content-wrapper">d
+        <div class="histories" id="content-wrapper">
+            <div class="histories" id="list"></div>
+            <div class="histories" id="pagination">
+                <a href="" class="histories" id="prev"></a>
+                <a href="" class="histories" id="next"></a>
+            </div>
+            <div class="histories" id="mode">
+                <div class="histories" id="summary">
+                    <div class="histories icon"></div>
+                    <div class="histories title">개요</div>
+                </div>
+                <div class="histories" id="custom">
+                    <div class="histories icon"></div>
+                    <div class="histories title">사용자 지정 모드</div>
+                </div>
+                <div class="histories" id="tournament">
+                    <div class="histories icon"></div>
+                    <div class="histories title">토너먼트 모드</div>
+                </div>
+            </div>
         </div>
         `;
     }
