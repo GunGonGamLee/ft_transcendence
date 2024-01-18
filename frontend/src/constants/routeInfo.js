ea99ee7fc399accc791b476196890bf5ff0bc68e
donghyun <donghyun1998@naver.com>
2024-01-18 16:44:24 +0900

    feat: errormessage ui 추가

--- frontend/src/constants/routeInfo.js
+++ frontend/src/constants/routeInfo.js
@@ -2,9 +2,9 @@ import GameMode from '../pages/game-mode/page.js';
 import Main from '../pages/main/page.js';
 import Nickname from '../pages/nickname/page.js';
 import histories from "../pages/histories/page.js";
-import errorHeader from '../header/errorHeader/header.js';
 import historiesHeader from '../header/historiesHeader/header.js';
 import nicknameHeader from '../header/nicknameHeader/header.js';
+import mainHeader from '../header/mainHeader/header.js';
 export const BASE_URL = 'http://localhost:3000';
 /**
  * 원하는 경로에 따라 렌더링할 컴포넌트를 정의합니다.
@@ -12,6 +12,6 @@ export const BASE_URL = 'http://localhost:3000';
 export const routes = [
   { path: /^\/$/, page: Main, header: nicknameHeader },
   { path: /^\/nickname$/, page: Nickname, header: nicknameHeader },
-  { path: /^\/gamemode$/, page: GameMode, header: errorHeader },
-    {path: /^\/histories$/, page: histories, header: historiesHeader}
+  { path: /^\/game-mode$/, page: GameMode, header: mainHeader },
+  { path: /^\/histories$/, page: histories, header: historiesHeader}
 ]
--- frontend/src/pages/nickname/page.js
+++ frontend/src/pages/nickname/page.js
@@ -5,8 +5,10 @@ import { navigate } from '../../utils/navigate.js';
  */
 export default function Nickname($container) {
   this.$container = $container;
-
+  this.errorMessage = '';
   this.setState = () => {
+    this.errorMessage = '이미 있다...'
+    // this.errorMessage = '최대 8글자 가능하다'
     this.render();
   };
 
@@ -17,12 +19,13 @@ export default function Nickname($container) {
       <div style="display: flex; flex-direction: column; align-items: center; margin-top: 10vh; width: 45vw; height: 50vh; background-color: #191D40; border: 2px solid white">
         <div style="margin-top: 10vh; text-align: center; color: white; font-size: 60px; font-family: Galmuri11, serif;">별명을 적어라</div>
         <input style="margin-top: 5vh; text-align: center; font-family: Galmuri11, serif; font-size: 45px; height: 10vh; width: 30vw" placeholder="최대 8글자 가능하다">
+        <div style="margin-top: 3vh; text-align: center; color: red; font-size: 20px; font-family: Galmuri11-Bold, serif;">${this.errorMessage}</div>
       </div>
     </div>
 	  `;
   };
 
-  this.render();
+  this.setState();
   $container.addEventListener('click', e => {
     if (e.target.id === 'button1') {
       navigate('/gamemode');
