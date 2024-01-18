30112a7d032f34d9286c199a2aafb208c341dd7d
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-18 14:16:24 +0900

    refactor(histories): 전적 리스트 렌더링하는 함수 분리

--- frontend/src/pages/histories/pages.js
+++ frontend/src/pages/histories/pages.js
@@ -43,55 +43,108 @@ export default function pages($container) {
         this.renderList();
     }
 
+    /**
+     * 사용자 지정 모드의 전적 리스트를 렌더링합니다.
+     * 1. 플레이어 1의 정보를 렌더링합니다.
+     * 2. 게임 모드(1 vs 1 로고 또는 토너먼트 로고)를 렌더링합니다.
+     * 3. 플레이어 2의 정보를 렌더링합니다.
+     */
     this.renderList = () => {
         let list = document.getElementById("list");
         list.innerHTML = `
-        <div class="histories" id="list-wrapper">asdfasdf</div>
+        <div class="histories" id="list-wrapper"></div>
         `
         let listWrapper = document.getElementById("list-wrapper");
         listWrapper.innerHTML = '';
         let mockData = [
             {
-                player1: "hyojocho",
-                player1_avatar: "../../../assets/images/avatar/red.png",
-                player1_rating: 2130,
-                player2: "yena",
-                player2_avatar: "../../../assets/images/avatar/blue.png",
-                player2_rating: 110,
-                winner: "hyojocho",
-            }
+                player1: {
+                    nickname: "hyojocho",
+                    avatar: "../../../assets/images/avatar/red.png",
+                    rating: 2130,
+                    is_winner: true,
+                },
+                player2: {
+                    nickname: "yena",
+                    avatar: "../../../assets/images/avatar/blue.png",
+                    rating: 110,
+                    is_winner: false,
+                },
+            },
+            {
+                player1: {
+                    nickname: "hyojocho",
+                    avatar: "../../../assets/images/avatar/red.png",
+                    rating: 2130,
+                    is_winner: true,
+                },
+                player2: {
+                    nickname: "yena",
+                    avatar: "../../../assets/images/avatar/blue.png",
+                    rating: 110,
+                    is_winner: false,
+                },
+            },
+            {
+                player1: {
+                    nickname: "hyojocho",
+                    avatar: "../../../assets/images/avatar/red.png",
+                    rating: 2130,
+                    is_winner: true,
+                },
+                player2: {
+                    nickname: "yena",
+                    avatar: "../../../assets/images/avatar/blue.png",
+                    rating: 110,
+                    is_winner: false,
+                },
+            },
+            {
+                player1: {
+                    nickname: "hyojocho",
+                    avatar: "../../../assets/images/avatar/red.png",
+                    rating: 2130,
+                    is_winner: true,
+                },
+                player2: {
+                    nickname: "donghyk2",
+                    avatar: "../../../assets/images/avatar/green.png",
+                    rating: 2120,
+                    is_winner: false,
+                },
+            },
         ]; // TODO: 백엔드로부터 데이터 받아오기
         for (let data of mockData) {
-            const listItemDiv = document.createElement("div");
-            listItemDiv.className = "histories list-item";
-            const player1Div = document.createElement("div");
-            player1Div.className = "histories player";
-            const gameModeDiv = document.createElement("div");
-            gameModeDiv.className = "histories game-mode";
-            const player2Div = document.createElement("div");
-            player2Div.className = "histories player";
-            player1Div.innerHTML = `
-            <div class="histories avatar">
-                <img class="histories" src="${data.player1_avatar}" alt="player1-avatar">
-            </div>
-            <div class="histories nickname">${data.player1}</div>
-            <div class="histories rating">Rating: ${data.player1_rating}</div>
-            `;
-            gameModeDiv.innerHTML = `
-            <img class="histories" src="../../../assets/images/1vs1_logo.png" alt="1v1">
-            `;
-            player2Div.innerHTML = `
-            <div class="histories avatar">
-                <img class="histories" src="${data.player2_avatar}" alt="player2-avatar">
-            </div>
-            <div class="histories nickname">${data.player2}</div>
-            <div class="histories rating">Rating: ${data.player2_rating}</div>
-            `;
-            listItemDiv.appendChild(player1Div);
-            listItemDiv.appendChild(gameModeDiv);
-            listItemDiv.appendChild(player2Div);
-            listWrapper.appendChild(listItemDiv);
+            const listItemA = document.createElement("a");
+            listItemA.className = "histories list-item";
+            listItemA.href = ""; // TODO: 전적 상세 페이지로 요청 보내기
+            this.renderPlayer(listItemA, data.player1);
+            this.renderGameMode(listItemA);
+            this.renderPlayer(listItemA, data.player2);
+            listWrapper.appendChild(listItemA);
         }
+    }
+
+    this.renderPlayer = (listItemA, data) => {
+        const playerDiv = document.createElement("div");
+        playerDiv.className = "histories player";
+        playerDiv.innerHTML = `
+        <div class="histories avatar">
+            <img class="histories" src="${data.avatar}" alt="player1-avatar">
+        </div>
+        <div class="histories nickname">${data.nickname}</div>
+        <div class="histories rating">Rating: ${data.rating}</div>
+        `;
+        listItemA.appendChild(playerDiv);
+    }
+
+    this.renderGameMode = (listItemA) => {
+        const gameModeDiv = document.createElement("div");
+        gameModeDiv.className = "histories game-mode";
+        gameModeDiv.innerHTML = `
+        <img class="histories" src="../../../assets/images/1vs1_logo.png" alt="1v1">
+        `;
+        listItemA.appendChild(gameModeDiv);
     }
 
     this.render();
