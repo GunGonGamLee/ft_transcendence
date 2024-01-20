4f1d538b123d5803a9192aa9251218d40c80dc8a
임원정 <wonlim@student.42seoul.kr>
2024-01-20 21:47:16 +0900

    feat: get 42 access token #71

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import FTLoginView, google_login, intra42_login, google_callback
+from .views import FTLoginView, google_login, intra42_login, google_callback, Intra42SignInCallBackView
 
 
 urlpatterns = [
     path('google/', google_login, name="google_login"), # 이 URL이 구글 로그인 선택창 (ID 선택창)
     path('intra42/', intra42_login, name="42_login"),
 
-	path('google/callback/', google_callback, name="google_callback"),
-	# path('42/callback/', ft_callback, name="ft_callback"),
-	
+    path('google/callback/', google_callback, name="google_callback"),
+    path('intra42/callback/', Intra42SignInCallBackView.as_view(), name="intra42_callback"),
+
     path('42/finish/', FTLoginView.as_view(), name="ft_login_finish"),
 ]
 
