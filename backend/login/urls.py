9a61f2d5e5449dc7dcadf19afebc92abc6caff9f
임원정 <wonlim@student.42seoul.kr>
2024-01-20 21:16:17 +0900

    feat: 42 login api 구현 #71

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import FTLoginView, google_login, google_callback
+from .views import FTLoginView, google_login, intra42_login, google_callback
 
 
 urlpatterns = [
     path('google/', google_login, name="google_login"), # 이 URL이 구글 로그인 선택창 (ID 선택창)
-    # path('42/', ft_login, name="login_42"),
+    path('intra42/', intra42_login, name="42_login"),
 
 	path('google/callback/', google_callback, name="google_callback"),
 	# path('42/callback/', ft_callback, name="ft_callback"),
