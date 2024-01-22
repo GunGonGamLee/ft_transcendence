7696d6d1f4c09173fe3a9c4889133d67856bb4fd
CHOI HYOJONG <50707297+bluedog129@users.noreply.github.com>
2024-01-22 18:16:28 +0900

    feat: login 페이지 추가 (#88)
    * feat: new version login페이지생성
    
    * feat : login wraper div태그 추가
    
    * fix : .login-wrapper 위치 중앙으로 고정
    
    * fix : .arcade-machine .top 클래스 영역 디자인 수정
    
    * fix : .screen-container 영역 디자인 수정
    
    * fix : .board 영역 디자인 수정
    
    * fix : 전체 디자인 수정
    
    ---------
    
    Co-authored-by: yena <50291995+nyj001012@users.noreply.github.com>

---
+++ frontend/src/pages/login/page.js
@@ -0,0 +1,88 @@
+import {click} from "../../utils/clickEvent.js";
+
+/**
+ * header 컴포넌트
+ * @param {HTMLElement} $container
+ */
+
+export default function Login($container) {
+    this.$container = $container;
+
+    this.setState = () => {
+        this.render();
+    }
+
+    this.render = () => {
+        this.renderLayout();
+    }
+
+    this.renderLayout = () => {
+        if (document.getElementsByTagName("head") !== null) {
+            document.getElementsByTagName("head")[0].insertAdjacentHTML(
+                "beforeend",
+                '<link rel="stylesheet" href="../../../assets/css/login.css"/>'
+            );
+        }
+        this.$container.innerHTML = `
+        <div class="login-wrapper">
+        <div class="container">
+        <div class="arcade-machine">
+          <div class="shadow"></div>
+          <div class="top">
+            <h1>FT_TRANCENDENCE</h1>
+          </div>
+          <div class="screen-container">
+            <div class="shadow"></div>
+            <div class="screen">
+              <div class="screen-display">
+              </div>
+              <h2 id="google">Google Login</h2>
+              <h2 id="forty-two">42 Login</h2>
+              <div class="alien-container">
+                <div class="alien">
+                  <div class="ear ear-left"></div>
+                  <div class="ear ear-right"></div>
+                  <div class="head-top"></div>
+                  <div class="head">
+                    <div class="eye eye-left"></div>
+                    <div class="eye eye-right"></div>
+                  </div>
+                  <div class="body"></div>
+                  <div class="arm arm-left"></div>
+                  <div class="arm arm-right"></div>
+                </div>
+              </div>
+            </div>
+            <div class="joystick">
+              <div class="stick"></div>
+            </div>
+          </div>
+          <div class="board">
+            <div class="button button-a"></div>
+            <div class="button button-b"></div>
+            <div class="button button-c"></div>
+          </div>
+          <div class="bottom">
+            <div class="stripes"></div>
+          </div>
+        </div>
+      </div>
+      </div>
+    `
+    }
+
+    this.addEventListenersToLayout = () => {
+        const google = document.getElementById("google");
+        const fortyTwo = document.getElementById("forty-two");
+        
+        click(google, () => {
+            window.location.href = "http://localhost:3000/api/auth/google";
+        });
+        click(fortyTwo, () => {
+            window.location.href = "http://localhost:3000/api/auth/42";
+        });
+    }
+
+    this.render();
+    this.addEventListenersToLayout();
+}
