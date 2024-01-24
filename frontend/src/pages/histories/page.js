6f6c6ff19badd4984ba5d8363bbc0abb930b96e4
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-24 14:12:55 +0900

    refactor(histories): toggle 변수명 및 html 태그 정리

--- frontend/src/pages/histories/page.js
+++ frontend/src/pages/histories/page.js
@@ -1,6 +1,6 @@
 import {hoverChangeColor, hoverChangeCursor, hoverChangeFont, hoverToggle} from "../../utils/hoverEvent.js";
 import {click} from "../../utils/clickEvent.js";
-import { importCss } from '../../utils/importCss.js';
+import {importCss} from '../../utils/importCss.js';
 import Summary from "./summary-page.js";
 import CustomHistories from "./custom-page.js";
 import TournamentHistories from "./tournament-page.js";
@@ -8,36 +8,36 @@ import Ranking from "./ranking-page.js";
 
 
 export default function Histories($container) {
-    this.$container = $container;
+  this.$container = $container;
 
-    this.setState = () => {
-        this.render();
-    }
+  this.setState = () => {
+    this.render();
+  }
 
-    this.render = () => {
-        this.renderLayout();
-        this.renderList();
-    }
+  this.render = () => {
+    this.renderLayout();
+    this.renderList();
+  }
 
-    /**
-     * 헤더, 그리고 개요-사용자 지정 모드-토너먼트 모드를 선택할 수 있는 버튼들을 렌더링합니다.
-     * 이는 전적 리스트의 페이지의 레이아웃으로 추후 공통 모듈로 분리할 수 있습니다.
-     */
-    this.renderLayout = () => {
-        importCss('../../../assets/css/histories.css')
-        this.$container.innerHTML = `
+  /**
+   * 헤더, 그리고 개요-사용자 지정 모드-토너먼트 모드를 선택할 수 있는 버튼들을 렌더링합니다.
+   * 이는 전적 리스트의 페이지의 레이아웃으로 추후 공통 모듈로 분리할 수 있습니다.
+   */
+  this.renderLayout = () => {
+    importCss('../../../assets/css/histories.css')
+    this.$container.innerHTML = `
         <div class="histories" id="content-wrapper">
             <nav class="histories" id="mode">
                 <div class="histories" id="summary" href="">
                     <img class="histories" src="../../../assets/images/custom_summary.png" alt="summary">
                     너의 기록은
                 </div>
-                <div class="histories title" id="custom" href="">
-                    <div class="histories" id="mode-select">
+                <div class="histories" id="custom-menu-wrapper" href="">
+                    <div class="histories custom-toggle" id="custom">
                         <img class="histories" src="../../../assets/images/setting.png" alt="custom-mode">
                         사용자 지정 모드
                     </div>
-                    <div class="histories" id="custom-toggle">
+                    <div class="histories custom-toggle" id="toggle">
                         <ul class="histories">
                             <li>1 vs 1 모드</li>
                             <li>토너먼트 모드</li>
@@ -66,38 +66,38 @@ export default function Histories($container) {
             </footer>
         </div>
         `;
-    }
+  }
 
-    this.renderList = () => {
-        new Summary(document.getElementById("list"));
-    }
+  this.renderList = () => {
+    new Summary(document.getElementById("list"));
+  }
 
   /**
    * 레이아웃 엘리먼트에 이벤트 리스너를 추가합니다.
    */
   this.addEventListenersToLayout = () => {
     const $summary = document.getElementById("summary");
+    const $customMenuWrapper = document.getElementById("custom-menu-wrapper");
     const $custom = document.getElementById("custom");
-    const $modeSelect = document.getElementById("mode-select");
-    const $toggle = Array.from(document.getElementsByTagName("li"));
+    const $toggleItems = Array.from(document.getElementsByTagName("li"));
     const $tournament = document.getElementById("tournament");
     const $ranking = document.getElementById("ranking");
     const $prev = document.getElementById("prev");
     const $next = document.getElementById("next");
 
     // 폰트 색상 변경
-    hoverChangeColor([$summary, $modeSelect, $tournament, $ranking], "#ffffff", "#29ABE2");
-    hoverChangeColor($toggle, "#aaaaaa", "#29ABE2");
+    hoverChangeColor([$summary, $custom, $tournament, $ranking], "#ffffff", "#29ABE2");
+    hoverChangeColor($toggleItems, "#aaaaaa", "#29ABE2");
 
     // 폰트 변경
-    hoverChangeFont([$summary, $modeSelect, $tournament, $ranking], "Galmuri11, serif", "Galmuri11-Bold, serif");
+    hoverChangeFont([$summary, $custom, $tournament, $ranking], "Galmuri11, serif", "Galmuri11-Bold, serif");
 
     // 커서 변경
-    hoverChangeCursor([$summary, $custom, $tournament, $ranking, $prev, $next], "pointer");
+    hoverChangeCursor([$summary, $customMenuWrapper, $tournament, $ranking, $prev, $next], "pointer");
 
     // click 이벤트
     click($summary, Summary);
-    click($custom, CustomHistories);
+    click($customMenuWrapper, CustomHistories);
     click($tournament, TournamentHistories);
     click($ranking, Ranking);
     click($prev, function () {
@@ -106,12 +106,14 @@ export default function Histories($container) {
     click($next, function () {
       console.log("TODO => 다음 페이지로 이동")
     });
-    let customToggle = document.getElementById("custom-toggle");
-    hoverToggle($custom, customToggle, "flex");
-    $custom.addEventListener("mouseover", () => {
-      $custom.style.color = "#29ABE2";
+
+    // toggle 이벤트
+    let $toggle = document.getElementById("toggle");
+    hoverToggle($customMenuWrapper, $toggle, "flex");
+    $customMenuWrapper.addEventListener("mouseover", () => {
+      $customMenuWrapper.style.color = "#29ABE2";
     })
   }
-    this.render();
-    this.addEventListenersToLayout();
+  this.render();
+  this.addEventListenersToLayout();
 }
