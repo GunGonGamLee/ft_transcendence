98b44b899dde4a8aa54ceca5579c625a985bd71e
chj <bluedog129@gmail.com>
2024-01-24 20:20:33 +0900

    feat : jwt토큰 저장 및 navigate 적용

--- frontend/src/pages/login/page.js
+++ frontend/src/pages/login/page.js
@@ -1,4 +1,5 @@
 import {click} from "../../utils/clickEvent.js";
+import { navigate } from "../../utils/navigate.js";
 
 /**
  * header 컴포넌트
@@ -76,20 +77,35 @@ export default function Login($container) {
         const fortyTwo = document.getElementById("forty-two");
         
         click(google, () => {
-          fetch('http://localhost:8000/api/login/google')
-          .then(response => response.json())
-          .then(data => {
-              window.location.href = data.url;
+          fetch('https://localhost:8000/api/login/google')
+          .then(response => {
+            if (response.status === 200) { // 성공 코드를 확인합니다.
+              return response.json();
+            } else {
+              throw new Error('Google login failed');
+            }
           })
-          .catch(error => console.error('google login error:', error));
+          .then(data => {
+            localStorage.setItem('jwt', data.token); // JWT 토큰을 저장합니다.
+            navigate('/register'); // 특정 페이지로 이동합니다.
+          })
+          .catch(error => console.error('Google login error:', error));
         });
+    
         click(fortyTwo, () => {
           fetch('http://localhost:8000/api/login/intra42')
-          .then(response => response.json())
-          .then(data => {
-              window.location.href = data.url;
+          .then(response => {
+            if (response.status === 200) { // 성공 코드를 확인합니다.
+              return response.json();
+            } else {
+              throw new Error('Intra42 login failed');
+            }
           })
-          .catch(error => console.error('intra42 login error:', error));
+          .then(data => {
+            localStorage.setItem('jwt', data.token); // JWT 토큰을 저장합니다.
+            navigate('/register'); // 특정 페이지로 이동합니다.
+          })
+          .catch(error => console.error('Intra42 login error:', error));
         });
     }
 
