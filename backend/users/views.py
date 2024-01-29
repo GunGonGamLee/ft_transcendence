588d01456e656fd5bb601cf64293f7033dcc57fa
임원정 <oddish0513@gmail.com>
2024-01-29 19:02:54 +0900

    feat: /users/me API 구현 #136

--- backend/users/views.py
+++ backend/users/views.py
@@ -11,6 +11,7 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
+from .serializers import UserInfoSerializer
 
 
 class SetNicknameView(APIView):
@@ -45,5 +46,24 @@ class SetNicknameView(APIView):
             return JsonResponse({'err_msg': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
         except IntegrityError:
             return JsonResponse({'err_msg': 'duplicate nickname'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return JsonResponse({'error': e.__class__.__name__}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class UserInfoView(APIView):
+    def get(self, request):
+        try:
+            user = AuthUtils.validate_jwt_token_and_get_user(request)
+            serializer = UserInfoSerializer(user)
+            print(serializer)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({'error': 'Token has expired'}, status=status.HTTP_401_UNAUTHORIZED)
+        except jwt.InvalidTokenError:
+            return JsonResponse({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+        except ValidationError as e:
+            return JsonResponse({'error': e.messages}, status=status.HTTP_401_UNAUTHORIZED)
+        except User.DoesNotExist:
+            return JsonResponse({'err_msg': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return JsonResponse({'error': e.__class__.__name__}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
