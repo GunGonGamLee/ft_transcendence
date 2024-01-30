e5075f498560d6dd5878f331dbb3c85bd763780b
yena <50291995+nyj001012@users.noreply.github.com>
2024-01-30 16:16:05 +0900

    style(histories): 코드 포맷팅

--- frontend/src/pages/histories/custom-page.js
+++ frontend/src/pages/histories/custom-page.js
@@ -1,6 +1,6 @@
-import {hoverChangeBorder, hoverChangeCursor} from "../../utils/hoverEvent.js";
-import {click} from "../../utils/clickEvent.js";
-import CustomHistoriesDetails from "./custom-histories-details.js";
+import { hoverChangeBorder, hoverChangeCursor } from '../../utils/hoverEvent.js';
+import { click } from '../../utils/clickEvent.js';
+import CustomHistoriesDetails from './custom-histories-details.js';
 
 /**
  * 사용자 지정 모드의 전적 리스트를 렌더링합니다.
@@ -8,52 +8,85 @@ import CustomHistoriesDetails from "./custom-histories-details.js";
  * @constructor 전적 리스트의 게임 모드
  */
 export default async function CustomHistories(mode) {
-  this.$customList = document.getElementById("content");
-  this.$pagination = document.getElementById("pagination");
+  this.$customList = document.getElementById('content');
+  this.$pagination = document.getElementById('pagination');
 
   this.init = () => {
-    this.$customList.textContent = "";
-    this.$pagination.style.display = "block";
+    this.$customList.textContent = '';
+    this.$pagination.style.display = 'block';
     this.mode = mode;
     this.needToRender = true;
-  }
+  };
 
   this.useState = async () => {
     // TODO => backend로부터 데이터 받아오기
-    if (mode === "1vs1") {
-      this.newState = [{
-        id: 1,
-        player1: {
-          nickname: "hyojocho", avatar: "../../../assets/images/avatar/red.png", rating: 2130, is_winner: true,
-        }, player2: {
-          nickname: "yena", avatar: "../../../assets/images/avatar/blue.png", rating: 110, is_winner: false,
+    if (mode === '1vs1') {
+      this.newState = [
+        {
+          id: 1,
+          player1: {
+            nickname: 'hyojocho',
+            avatar: '../../../assets/images/avatar/red.png',
+            rating: 2130,
+            is_winner: true,
+          },
+          player2: {
+            nickname: 'yena',
+            avatar: '../../../assets/images/avatar/blue.png',
+            rating: 110,
+            is_winner: false,
+          },
         },
-      }, {
-        id: 2,
-        player1: {
-          nickname: "hyojocho", avatar: "../../../assets/images/avatar/red.png", rating: 2130, is_winner: true,
-        }, player2: {
-          nickname: "yena", avatar: "../../../assets/images/avatar/blue.png", rating: 110, is_winner: false,
+        {
+          id: 2,
+          player1: {
+            nickname: 'hyojocho',
+            avatar: '../../../assets/images/avatar/red.png',
+            rating: 2130,
+            is_winner: true,
+          },
+          player2: {
+            nickname: 'yena',
+            avatar: '../../../assets/images/avatar/blue.png',
+            rating: 110,
+            is_winner: false,
+          },
         },
-      }, {
-        id: 3,
-        player1: {
-          nickname: "hyojocho", avatar: "../../../assets/images/avatar/red.png", rating: 2130, is_winner: true,
-        }, player2: {
-          nickname: "yena", avatar: "../../../assets/images/avatar/blue.png", rating: 110, is_winner: false,
+        {
+          id: 3,
+          player1: {
+            nickname: 'hyojocho',
+            avatar: '../../../assets/images/avatar/red.png',
+            rating: 2130,
+            is_winner: true,
+          },
+          player2: {
+            nickname: 'yena',
+            avatar: '../../../assets/images/avatar/blue.png',
+            rating: 110,
+            is_winner: false,
+          },
         },
-      }, {
-        id: 4,
-        player1: {
-          nickname: "hyojocho", avatar: "../../../assets/images/avatar/red.png", rating: 2130, is_winner: true,
-        }, player2: {
-          nickname: "donghyk2", avatar: "../../../assets/images/avatar/green.png", rating: 2120, is_winner: false,
+        {
+          id: 4,
+          player1: {
+            nickname: 'hyojocho',
+            avatar: '../../../assets/images/avatar/red.png',
+            rating: 2130,
+            is_winner: true,
+          },
+          player2: {
+            nickname: 'donghyk2',
+            avatar: '../../../assets/images/avatar/green.png',
+            rating: 2120,
+            is_winner: false,
+          },
         },
-      },];
-    } else if (mode === "tournament") {
+      ];
+    } else if (mode === 'tournament') {
       // TODO => 토너먼트 모드 데이터 받아오기
     }
-  }
+  };
 
   this.setState = () => {
     if (this.state === this.newState) {
@@ -62,7 +95,7 @@ export default async function CustomHistories(mode) {
     }
     this.state = this.newState;
     this.needToRender = true;
-  }
+  };
 
   /**
    * 사용자 지정 모드의 전적 리스트를 렌더링합니다.
@@ -71,48 +104,54 @@ export default async function CustomHistories(mode) {
    * 3. 플레이어 2의 정보를 렌더링합니다.
    */
   this.render = () => {
-    this.$customList.insertAdjacentHTML("afterbegin", `
+    this.$customList.insertAdjacentHTML(
+      'afterbegin',
+      `
         <div class="histories" id="list-wrapper"></div>
-        `);
-    let $listWrapper = document.getElementById("list-wrapper");
-    if (mode === "1vs1") {
+        `,
+    );
+    let $listWrapper = document.getElementById('list-wrapper');
+    if (mode === '1vs1') {
       this.render1vs1($listWrapper);
-    } else if (mode === "tournament") {
-      console.log("TODO => 토너먼트 모드")
+    } else if (mode === 'tournament') {
+      console.log('TODO => 토너먼트 모드');
       // this.renderTournament($listWrapper);
     }
-  }
+  };
 
   /**
    * 사용자 지정 게임의 1 vs 1 모드 전적 리스트를 렌더링합니다.
    * @param $listWrapper {HTMLElement} 전적 리스트를 렌더링할 <div> 엘리먼트
    */
-  this.render1vs1 = ($listWrapper) => {
+  this.render1vs1 = $listWrapper => {
     for (let data of this.state) {
-      const {id, player1, player2} = data;
-      $listWrapper.insertAdjacentHTML("beforeend", `
+      const { id, player1, player2 } = data;
+      $listWrapper.insertAdjacentHTML(
+        'beforeend',
+        `
         <div class="histories list-item" data-item-id="${id}">
             ${this.renderPlayer(player1)}
             ${this.renderGameMode()}
             ${this.renderPlayer(player2)}
         </div>
-      `);
+      `,
+      );
       let $listItemDiv = $listWrapper.lastElementChild;
-      hoverChangeBorder($listItemDiv, "3px solid transparent", "3px solid #29ABE2");
-      hoverChangeCursor($listItemDiv, "pointer");
+      hoverChangeBorder($listItemDiv, '3px solid transparent', '3px solid #29ABE2');
+      hoverChangeCursor($listItemDiv, 'pointer');
       click($listItemDiv, () => {
-        CustomHistoriesDetails.bind(this, id, "1vs1")();
+        CustomHistoriesDetails.bind(this, id, '1vs1')();
       });
       $listWrapper.appendChild($listItemDiv);
     }
-  }
+  };
 
   /**
    * 전적 리스트의 플레이어 정보를 렌더링합니다.
    * @param data {{avatar: string, nickname: string, rating: string}} 전적 리스트의 플레이어 정보
    */
-  this.renderPlayer = (data) => {
-    return (`
+  this.renderPlayer = data => {
+    return `
       <div class="histories player">
         <div class="histories avatar">
             <img class="histories" src="${data.avatar}" alt="player1-avatar">
@@ -120,22 +159,22 @@ export default async function CustomHistories(mode) {
         <div class="histories nickname">${data.nickname}</div>
         <div class="histories rating">Rating: ${data.rating}</div>
       </div>
-    `);
-  }
+    `;
+  };
 
   /**
    * 전적 리스트의 게임 모드(1 vs 1 로고 또는 토너먼트 로고)를 렌더링합니다.
    */
   this.renderGameMode = () => {
-    if (this.mode === "1vs1") {
+    if (this.mode === '1vs1') {
       return `
         <div class="histories game-mode">
             <img class="histories" src="../../../assets/images/1vs1_logo.png" alt="1v1">
         </div>
       `;
-    } else if (this.mode === "tournament") {
+    } else if (this.mode === 'tournament') {
     }
-  }
+  };
 
   this.init();
   await this.useState();
--- frontend/src/pages/histories/tournament-page.js
+++ frontend/src/pages/histories/tournament-page.js
@@ -1,13 +1,13 @@
 export default async function TournamentHistories(isCustomMode) {
-  this.$pagination = document.getElementById("pagination");
+  this.$pagination = document.getElementById('pagination');
   this.needToRender = true;
-  this.imagePath = "../../../assets/images";
+  this.imagePath = '../../../assets/images';
   this.avatarPath = `${this.imagePath}/avatar`;
 
   this.init = () => {
-    this.textContent = "";
-    this.$pagination.style.display = "block";
-  }
+    this.textContent = '';
+    this.$pagination.style.display = 'block';
+  };
 
   /**
    * 토너먼트 모드에 대한 데이터를 받아옵니다. 만약 isCustomMode가 true라면 사용자 지정 모드의 토너먼트 모드에 대한 데이터를 받아옵니다.
@@ -15,112 +15,112 @@ export default async function TournamentHistories(isCustomMode) {
    * @param isCustomMode {boolean} 사용자 지정 모드인지 아닌지에 대한 boolean 값.
    * @returns {Promise<void>} 토너먼트 모드에 대한 데이터.
    */
-  this.useState = async (isCustomMode) => {
+  this.useState = async isCustomMode => {
     // TODO => 백엔드에서 토너먼트 모드에 대한 데이터를 받아오기
     this.newState = [
       {
         id: 1,
         player: {
-          nickname: "hyojocho",
-          avatar: "blue_bust",
+          nickname: 'hyojocho',
+          avatar: 'blue_bust',
           ranking: 1,
         },
         opponents: [
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 2,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 3,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 3,
           },
         ],
-        date: "2023.01.30 "
+        date: '2023.01.30 ',
       },
       {
         id: 2,
         player: {
-          nickname: "hyojocho",
-          avatar: "blue_bust",
+          nickname: 'hyojocho',
+          avatar: 'blue_bust',
           ranking: 3,
         },
         opponents: [
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 3,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 2,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 1,
           },
-        ]
+        ],
       },
       {
         id: 3,
         player: {
-          nickname: "hyojocho",
-          avatar: "blue_bust",
+          nickname: 'hyojocho',
+          avatar: 'blue_bust',
           ranking: 3,
         },
         opponents: [
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 3,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 2,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 1,
           },
-        ]
+        ],
       },
       {
         id: 4,
         player: {
-          nickname: "hyojocho",
-          avatar: "blue_bust",
+          nickname: 'hyojocho',
+          avatar: 'blue_bust',
           ranking: 3,
         },
         opponents: [
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 3,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 2,
           },
           {
-            nickname: "yena",
-            avatar: "green_bust",
+            nickname: 'yena',
+            avatar: 'green_bust',
             ranking: 1,
           },
-        ]
-      }
+        ],
+      },
     ];
-  }
+  };
 
   this.setState = () => {
     if (this.state !== this.newState) {
@@ -129,32 +129,32 @@ export default async function TournamentHistories(isCustomMode) {
     } else {
       this.needToRender = false;
     }
-  }
+  };
 
   /**
    * 랭킹을 렌더링합니다.
    * @param ranking {number} 랭킹.
    * @returns {string} 랭킹을 렌더링하는 HTML.
    */
-  this.renderRanking = (ranking) => {
-    let html = (`
+  this.renderRanking = ranking => {
+    let html = `
     <div class="histories tournament ranking">
       <div class="histories tournament player-ranking first">
-    `);
+    `;
     if (ranking === 1) {
-      html += (`
+      html += `
         <img class="histories" src="${this.imagePath}/winner.png" alt="first">
-      `);
+      `;
     }
-    html += (`
+    html += `
       </div>
       <div class="histories ranking-text">
         ${ranking}등
       </div>
     </div>
-    `);
+    `;
     return html;
-  }
+  };
 
   /**
    * 플레이어를 렌더링합니다.
@@ -163,8 +163,8 @@ export default async function TournamentHistories(isCustomMode) {
    * @param ranking {number} 플레이어의 랭킹.
    * @returns {string} 플레이어를 렌더링하는 HTML.
    */
-  this.renderPlayer = ({nickname, avatar, ranking}) => {
-    return (`
+  this.renderPlayer = ({ nickname, avatar, ranking }) => {
+    return `
     <div class="histories tournament player">
       <div class="histories tournament player-avatar">
         <img class="histories" src="${this.avatarPath}/${avatar}.png" alt="avatar">
@@ -173,64 +173,70 @@ export default async function TournamentHistories(isCustomMode) {
         ${nickname}
       </div>
     </div>
-    `);
-  }
+    `;
+  };
 
   /**
    * 토너먼트 모드에서 상대방들을 렌더링합니다.
    * @param opponents {Array<{nickname: string, avatar: string, ranking: number}>} 상대방들의 정보.
    * @returns {string} 상대방들을 렌더링하는 HTML.
    */
-  this.renderOpponents = (opponents) => {
-    let html = (`<div class= "histories tournament opponents">`);
+  this.renderOpponents = opponents => {
+    let html = `<div class= "histories tournament opponents">`;
     for (let opponent of opponents) {
-      html += (`
+      html += `
       <div class="histories tournament opponents-avatar">
         <img class="histories" src = "${this.avatarPath}/${opponent.avatar}.png" alt="avatar">
       </div>
       <div class="histories tournament opponents-nickname">
         ${opponent.nickname}
         <div class="histories tournament opponents-ranking first">
-      `);
+      `;
       if (opponent.ranking === 1) {
-        html += (`
+        html += `
           <img class="histories" src="${this.imagePath}/winner.png" alt="first">
-       `);
+       `;
       }
-      html += (`
+      html += `
         </div>
       </div>
-      `);
+      `;
     }
-    html += (`</div>`);
+    html += `</div>`;
     return html;
-  }
+  };
 
   /**
    * 토너먼트 모드에서 리스트를 렌더링합니다.
    */
   this.renderList = () => {
-    let $listWrapper = document.getElementById("list-wrapper");
+    let $listWrapper = document.getElementById('list-wrapper');
     for (let data of this.state) {
-      let $listItem = document.createElement("div");
-      $listItem.classList.add("histories");
-      $listItem.classList.add("tournament");
-      $listItem.classList.add("list-item");
-      $listItem.insertAdjacentHTML("afterbegin", `
+      let $listItem = document.createElement('div');
+      $listItem.classList.add('histories');
+      $listItem.classList.add('tournament');
+      $listItem.classList.add('list-item');
+      $listItem.insertAdjacentHTML(
+        'afterbegin',
+        `
         ${this.renderRanking(data.player.ranking)}
         ${this.renderPlayer(data.player)}
         ${this.renderOpponents(data.opponents)}
-      `)
+      `,
+      );
       $listWrapper.appendChild($listItem);
     }
-  }
+  };
 
   this.render = () => {
-    this.insertAdjacentHTML("afterbegin", `
+    this.insertAdjacentHTML(
+      'afterbegin',
+      `
       <div class="histories" id="list-wrapper"></div>
-    `)
+    `,
+    );
     this.renderList();
-  }
+  };
 
   this.init();
   await this.useState(isCustomMode);
