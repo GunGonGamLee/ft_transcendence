2d0042d036077c263c359ed26e9b916cdf7ad2fc
donghyun <donghyun1998@naver.com>
2024-01-30 20:27:03 +0900

    feat: roomSettingModal form/input 태그로 변경

--- frontend/src/pages/waiting-room/roomSettingModal.js
+++ frontend/src/pages/waiting-room/roomSettingModal.js
@@ -8,21 +8,19 @@ export default function roomSettingModal(isVisible) {
               <div style="background: rgba(251.14, 255, 62, 0.20); border-radius: 10px; border: 5px #FBFF3E solid; width: 100%; height: 100%;"></div>
               <div style="position: absolute; left: 48px; top: 9px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">수정</div>
           </div>
-          <div style="position: absolute; width: 753px; height: 62px; left: 64px; top: 300px;">
-              <div style="position: absolute; top: 11px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">암호</div>
-              <div style="position: absolute; width: 553px; height: 62px; left: 200px; border-radius: 10px; border: 3px white solid;"></div>
-              <div style="position: absolute; left: 214px; top: 10px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">1234pass</div>
-          </div>
+          <form action="/submit-url" method="post" style="position: relative; width: 753px; height: 62px; left: 64px; top: 300px;">
+            <div style="position: absolute; top: 11px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">암호</div>
+            <input type="password" name="password" value="1234pass" style="position: absolute; width: 553px; height: 62px; left: 200px; border-radius: 10px; border: 3px white solid; background: transparent; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400; padding-left: 14px;">
+          </form>
           <div style="position: absolute; width: 528px; height: 51px; left: 60px; top: 223px;">
-              <div style="position: absolute; left: 384px; top: 3px; opacity: 0.50; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">토너먼트</div>
-              <div style="position: absolute; left: 202px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">1 vs 1</div>
-              <div style="position: absolute; top: 3px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">모드 선택</div>
+            <div style="position: absolute; left: 384px; top: 3px; opacity: 0.50; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">토너먼트</div>
+            <div style="position: absolute; left: 202px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">1 vs 1</div>
+            <div style="position: absolute; top: 3px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">모드 선택</div>
           </div>
-          <div style="position: absolute; width: 751px; height: 62px; left: 66px; top: 126px;">
-              <div style="position: absolute; top: 10px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">방 이름</div>
-              <div style="position: absolute; width: 553px; height: 62px; left: 198px; border-radius: 10px; border: 3px white solid;"></div>
-              <div style="position: absolute; left: 213px; top: 10px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">Zㅣ존 트센</div>
-          </div>
+          <form action="/submit-url" method="post" style="position: absolute; width: 751px; height: 62px; left: 66px; top: 126px;">
+            <div style="position: absolute; top: 10px; text-align: center; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400;">방 이름</div>
+            <input type="text" name="roomName" value="Zㅣ존 트센" style="position: absolute; width: 553px; height: 62px; left: 198px; border-radius: 10px; border: 3px white solid; background: transparent; color: white; font-size: 36px; font-family: Galmuri11, serif; font-weight: 400; padding-left: 14px;">
+          </form>
           <img alt="setting" style="position: absolute; width: 70px; height: 70px; left: 770px; top: 26px;" src="../../../assets/images/close.png" />
       </div>
     `;
