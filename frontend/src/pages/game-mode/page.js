aa3ac8e83e0bf2c85d702dd4aeca9390dd32f8ce
donghyun kim <donghyun1998@naver.com>
2024-01-31 15:20:29 +0900

    feat: waiting room 마저 구현 (#144)
    * fix: 배경화면 이미지 제대로 적용되게 고쳐줌
    
    * feat: modal ui 구현
    
    * chore: errorModal x버튼 모양 정사각형으로
    
    * feat: roomSettingModal form/input 태그로 변경
    
    * chore: form태그에서 다시 div태그로 변경
    
    이거 생각해보니까 수정 버튼 눌러야지 바뀌는거네 ㅋㅋ
    
    * fix: spacer 를 space-evenly로 변경 신문물 ㄷ
    
    * chore: waiting-room/roomSettingModal input 최대 10글자로

--- frontend/src/pages/game-mode/page.js
+++ frontend/src/pages/game-mode/page.js
@@ -1,6 +1,6 @@
-import { navigate } from '../../utils/navigate.js';
-import { importCss } from '../../utils/importCss.js';
-import gameModeUnit from './gameModeUnit.js';
+import { navigate } from "../../utils/navigate.js";
+import { importCss } from "../../utils/importCss.js";
+import gameModeUnit from "./gameModeUnit.js";
 /**
  * @param {HTMLElement} $container
  */
@@ -12,22 +12,20 @@ export default function GameMode($container) {
   };
 
   this.render = () => {
-    importCss('../../../assets/css/game-mode.css')
+    importCss("../../../assets/css/game-mode.css");
     this.$container.innerHTML = `
         <div class="game-mode-container">
         ${gameModeUnit(0)}
-        <div class="spacer"></div>
         ${gameModeUnit(1)}
-        <div class="spacer"></div>
         ${gameModeUnit(2)}
     </div>
 	  `;
   };
 
   this.render();
-  $container.addEventListener('click', e => {
-    if (e.target.id === 'button2') {
-      navigate('/');
+  $container.addEventListener("click", (e) => {
+    if (e.target.id === "button2") {
+      navigate("/");
     }
   });
 }
