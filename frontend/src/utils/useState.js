f26c5c1328d1c67e4e3836aaae0ffbc47fc09f55
donghyun <donghyun1998@naver.com>
2024-02-01 17:50:03 +0900

    chore: jsdoc 타입 object도 추가

--- frontend/src/utils/useState.js
+++ frontend/src/utils/useState.js
@@ -1,6 +1,6 @@
 /**
  * @description useState 훅 구현
- * @param {array | number | string}stateInput 상태
+ * @param {array | number | string | boolean | Object}stateInput 상태
  * @param {object}component 전달된 컴포넌트
  * @param {string}render 렌더링 함수 명
  * @returns [getState, setState] 반환
@@ -17,7 +17,7 @@ export default function useState(stateInput, component, render) {
       component[render](); // 그냥 render를 인자로 받으면 최신화가 안되버리는..!
     };
     return [getState, setState];
-  } else if (typeof stateInput === "number" || typeof stateInput === "string") {
+  } else {
       let state = stateInput;
       const getState = () => {
         return state;
