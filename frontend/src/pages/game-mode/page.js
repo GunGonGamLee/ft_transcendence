a82e2387781cf7d1b637e2ae18febea3609e4683
donghyun <donghyun1998@naver.com>
2024-02-01 15:24:08 +0900

    feat: useState 훅 구현

--- frontend/src/pages/game-mode/page.js
+++ frontend/src/pages/game-mode/page.js
@@ -1,12 +1,13 @@
-import { navigate } from "../../utils/navigate.js";
 import { importCss } from "../../utils/importCss.js";
 import gameModeUnit from "./gameModeUnit.js";
+import useState from "../../utils/useState.js";
 /**
  * @param {HTMLElement} $container
  */
+
 export default function GameMode($container) {
   this.$container = $container;
-
+  let [getGameMode, setGameMode] = useState(0, this);
   this.setState = () => {
     this.render();
   };
@@ -15,17 +16,18 @@ export default function GameMode($container) {
     importCss("../../../assets/css/game-mode.css");
     this.$container.innerHTML = `
         <div class="game-mode-container">
-        ${gameModeUnit(0)}
-        ${gameModeUnit(1)}
-        ${gameModeUnit(2)}
-    </div>
+          ${gameModeUnit(getGameMode())}
+          ${gameModeUnit(getGameMode())}
+          ${gameModeUnit(getGameMode())}
+          <button id="button2" style="position: absolute; bottom: 10px; right: 10px;">state</button>
+        </div>
 	  `;
   };
 
   this.render();
   $container.addEventListener("click", (e) => {
     if (e.target.id === "button2") {
-      navigate("/");
+      setGameMode(getGameMode() + 1);
     }
   });
 }
