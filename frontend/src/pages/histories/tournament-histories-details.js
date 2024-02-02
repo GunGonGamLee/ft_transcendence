edf3e0f4ddf7e5d3c5b62c1454606b389b5fd2ef
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-02 13:26:19 +0900

    feat(histories): 페이지네이션 버튼 클릭할 때 그에 맞는 내용 렌더링하는 함수 연결

--- frontend/src/pages/histories/tournament-histories-details.js
+++ frontend/src/pages/histories/tournament-histories-details.js
@@ -1,6 +1,14 @@
+import { click, onClick } from "../../utils/clickEvent.js";
+
 export default async function TournamentHistoriesDetails(id) {
+  this.needToRender = true;
+
   this.init = () => {
     this.textContent = "";
+    this.$prev = document.getElementById("prev");
+    this.$next = document.getElementById("next");
+    onClick(this.$prev, this.renderTournamentTree);
+    onClick(this.$next, this.renderTournamentResult);
   };
 
   this.useState = async () => {
@@ -10,47 +18,66 @@ export default async function TournamentHistoriesDetails(id) {
         player1: {
           avatar: "luke_skywalkrer",
           nickname: "hyojocho",
-          rating: 1234
+          rating: 1234,
         },
         player2: {
           avatar: "luke_skywalkrer",
           nickname: "hyojocho",
-          rating: 110
-        }
+          rating: 110,
+        },
       },
       match2: {
         player1: {
           avatar: "luke_skywalkrer",
           nickname: "hyojocho",
-          rating: 110
-        }
-        ,
+          rating: 110,
+        },
         player2: {
           avatar: "luke_skywalkrer",
           nickname: "hyojocho",
-          rating: 1234
-        }
+          rating: 1234,
+        },
       },
       match3: {
         player1: {
           avatar: "luke_skywalkrer",
           nickname: "hyojocho",
-          rating: 1234
+          rating: 1234,
         },
         player2: {
           avatar: "luke_skywalkrer",
           nickname: "hyojocho",
-          rating: 1234
+          rating: 1234,
         },
         winner: "yena",
-        date: "2023.02.01 13:30:23"
-      }
+        date: "2023.02.01 13:30:23",
+      },
+    };
+  };
+
+  this.setState = () => {
+    if (this.newState !== this.state) {
+      this.state = this.newState;
+      this.needToRender = true;
+    } else this.needToRender = false;
+  };
+
+  this.render = () => {
+    if (this.needToRender) {
+      this.renderTournamentTree();
     }
-    ;
+  };
 
-    this.setState = () => {
-    };
+  this.renderTournamentTree = () => {
+    console.log("TODO => 토너먼트 트리 렌더링");
+  };
 
-    this.render = () => {
-    };
-  }
+  this.renderTournamentResult = () => {
+    console.log("TODO => 토너먼트 결과 렌더링");
+  };
+
+  this.init();
+  await this.useState();
+  this.setState();
+  this.render();
+}
