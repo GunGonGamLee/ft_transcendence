5edf40d6fd7e1bc3c3488b3dd03b719797bb1618
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-02 13:13:57 +0900

    feat(histories): mock data 추가

--- frontend/src/pages/histories/tournament-histories-details.js
+++ frontend/src/pages/histories/tournament-histories-details.js
@@ -3,9 +3,54 @@ export default async function TournamentHistoriesDetails(id) {
     this.textContent = "";
   };
 
-  this.useState = () => {};
+  this.useState = async () => {
+    // TODO => 백엔드에서 토너먼트 모드에 대한 데이터를 받아오기
+    this.newState = {
+      match1: {
+        player1: {
+          avatar: "luke_skywalkrer",
+          nickname: "hyojocho",
+          rating: 1234
+        },
+        player2: {
+          avatar: "luke_skywalkrer",
+          nickname: "hyojocho",
+          rating: 110
+        }
+      },
+      match2: {
+        player1: {
+          avatar: "luke_skywalkrer",
+          nickname: "hyojocho",
+          rating: 110
+        }
+        ,
+        player2: {
+          avatar: "luke_skywalkrer",
+          nickname: "hyojocho",
+          rating: 1234
+        }
+      },
+      match3: {
+        player1: {
+          avatar: "luke_skywalkrer",
+          nickname: "hyojocho",
+          rating: 1234
+        },
+        player2: {
+          avatar: "luke_skywalkrer",
+          nickname: "hyojocho",
+          rating: 1234
+        },
+        winner: "yena",
+        date: "2023.02.01 13:30:23"
+      }
+    }
+    ;
 
-  this.setState = () => {};
+    this.setState = () => {
+    };
 
-  this.render = () => {};
-}
+    this.render = () => {
+    };
+  }
