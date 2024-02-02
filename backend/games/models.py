a75e5107362ae5c5e1c41756e34a4232c0c6a731
임원정 <wonlim@student.42seoul.kr>
2024-02-02 16:48:30 +0900

    feat: model 수정 #157
    player model 삭제,  game table 구조 변경

--- backend/games/models.py
+++ backend/games/models.py
@@ -1,5 +1,4 @@
 from django.db import models, connection
-# from django_db_views.db_view import DBView
 from users.models import User
 
 
@@ -21,7 +20,9 @@ class Game(models.Model):
     password = models.CharField(null=True, blank=True)
     status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES)
     manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    players = models.ForeignKey('Player', on_delete=models.SET_NULL, null=True)
+    player1 = models.ForeignKey(User, related_name='player1_id', on_delete=models.SET_NULL, null=True, blank=True)
+    player2 = models.ForeignKey(User, related_name='player2_id', on_delete=models.SET_NULL, null=True, blank=True)
+    player3 = models.ForeignKey(User, related_name='player3_id', on_delete=models.SET_NULL, null=True, blank=True)
     match1 = models.ForeignKey('Result', related_name='result_match1', on_delete=models.SET_NULL, null=True, blank=True)
     match2 = models.ForeignKey('Result', related_name='result_match2', on_delete=models.SET_NULL, null=True, blank=True)
     match3 = models.ForeignKey('Result', related_name='result_match3', on_delete=models.SET_NULL, null=True, blank=True)
@@ -31,20 +32,10 @@ class Game(models.Model):
         db_table = 'games'
 
 
-class Player(models.Model):
-    player1 = models.ForeignKey(User, related_name='player1_id', on_delete=models.SET_NULL, null=True, blank=True)
-    player2 = models.ForeignKey(User, related_name='player2_id', on_delete=models.SET_NULL, null=True, blank=True)
-    player3 = models.ForeignKey(User, related_name='player3_id', on_delete=models.SET_NULL, null=True, blank=True)
-    player4 = models.ForeignKey(User, related_name='player4_id', on_delete=models.SET_NULL, null=True, blank=True)
-
-    class Meta:
-        db_table = 'players'
-
-
 class Result(models.Model):
     winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    player1 = models.ForeignKey(Player, related_name='result_player1', on_delete=models.SET_NULL, null=True, blank=True)
-    player2 = models.ForeignKey(Player, related_name='result_player2', on_delete=models.SET_NULL, null=True, blank=True)
+    player1 = models.ForeignKey(User, related_name='result_player1', on_delete=models.SET_NULL, null=True, blank=True)
+    player2 = models.ForeignKey(User, related_name='result_player2', on_delete=models.SET_NULL, null=True, blank=True)
     player1_score = models.PositiveSmallIntegerField(null=True, blank=True)
     player2_score = models.PositiveSmallIntegerField(null=True, blank=True)
     started_at = models.DateTimeField(null=True, blank=True)
@@ -102,28 +93,28 @@ class CasualGameView(models.Model):
         managed = False
 
 
-class GameRecordViewManager(models.Manager):
-    def get_queryset(self):
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                SELECT mode, g.id, p.player1_id, p.player2_id, p.player3_id, p.player4_id
-                from game g
-                inner join player p on g.players_id = p.id;
-            """)
-            results = cursor.fetchall()
+# class GameRecordViewManager(models.Manager):
+#     def get_queryset(self):
+#         with connection.cursor() as cursor:
+#             cursor.execute("""
+#                 SELECT mode, g.id, p.player1_id, p.player2_id, p.player3_id, p.player4_id
+#                 from game g
+#                 inner join player p on g.players_id = p.id;
+#             """)
+#             results = cursor.fetchall()
+#
+#         return [self.model(*row) for row in results]
 
-        return [self.model(*row) for row in results]
 
-
-class GameRecordView(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True, blank=True)
-    mode = models.PositiveSmallIntegerField()
-    player1 = models.ForeignKey(Player, related_name='gamerecordview_player1', on_delete=models.SET_NULL, null=True, blank=True)
-    player2 = models.ForeignKey(Player, related_name='gamerecordview_player2', on_delete=models.SET_NULL, null=True, blank=True)
-    player3 = models.ForeignKey(Player, related_name='gamerecordview_player3', on_delete=models.SET_NULL, null=True, blank=True)
-    player4 = models.ForeignKey(Player, related_name='gamerecordview_player4', on_delete=models.SET_NULL, null=True, blank=True)
-
-    objects = GameRecordViewManager()
-
-    class Meta:
-        managed = False
+# class GameRecordView(models.Model):
+#     game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True, blank=True)
+#     mode = models.PositiveSmallIntegerField()
+#     player1 = models.ForeignKey(Player, related_name='gamerecordview_player1', on_delete=models.SET_NULL, null=True, blank=True)
+#     player2 = models.ForeignKey(Player, related_name='gamerecordview_player2', on_delete=models.SET_NULL, null=True, blank=True)
+#     player3 = models.ForeignKey(Player, related_name='gamerecordview_player3', on_delete=models.SET_NULL, null=True, blank=True)
+#     player4 = models.ForeignKey(Player, related_name='gamerecordview_player4', on_delete=models.SET_NULL, null=True, blank=True)
+#
+#     objects = GameRecordViewManager()
+#
+#     class Meta:
+#         managed = False
