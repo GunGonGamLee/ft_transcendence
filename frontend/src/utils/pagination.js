3c59ee1beeca2508d11c9ae58bde8d2eb3ab6718
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-02 13:37:10 +0900

    style(utils): jsdoc 추가

--- frontend/src/utils/pagination.js
+++ frontend/src/utils/pagination.js
@@ -1,5 +1,12 @@
 import { click, onClick } from "./clickEvent.js";
 
+/**
+ * 이전, 다음 페이지로 이동하는 버튼에 대한 이벤트 리스너를 추가합니다.
+ * @param prevId {string} 이전 페이지로 이동하는 버튼의 id
+ * @param nextId {string} 다음 페이지로 이동하는 버튼의 id
+ * @param prevFunction {function} 이전 페이지 클릭 시 동작하는 함수
+ * @param nextFunction {function} 다음 페이지 클릭 시 동작하는 함수
+ */
 export const addPaginationClickListeners = (
   prevId,
   nextId,
@@ -12,6 +19,13 @@ export const addPaginationClickListeners = (
   click($next, nextFunction);
 };
 
+/**
+ * 이전, 다음 페이지로 이동하는 버튼에 대한 onClick 프로퍼티를 추가합니다.
+ * @param prevId {string} 이전 페이지로 이동하는 버튼의 id
+ * @param nextId {string} 다음 페이지로 이동하는 버튼의 id
+ * @param prevFunction {function} 이전 페이지 클릭 시 동작하는 함수
+ * @param nextFunction {function} 다음 페이지 클릭 시 동작하는 함수
+ */
 export const addPaginationOnClickProperty = (
   prevId,
   nextId,
