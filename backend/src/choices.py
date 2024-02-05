3970988d856c041262d12753aeec71499359d7ba
임원정 <wonlim@student.42seoul.kr>
2024-02-05 17:32:54 +0900

    refactor: 코드 리팩토링
    choices를 관리하기 쉽게 한 파일에 저장

---
+++ backend/src/choices.py
@@ -0,0 +1,14 @@
+AVATAR_CHOICES_DICT = {
+    0: "chewbacca.png",
+    1: "darth_vader.png",
+    2: "luke_skywalker.png",
+    3: "han_solo.png",
+    4: "yoda.png",
+}
+
+MODE_CHOICES_DICT = {
+    0: "casual_1vs1",
+    1: "casual_tournament",
+    2: "rank",
+    3: "local_tournament",
+}
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
 from users.models import User
+from src.choices import MODE_CHOICES_DICT, AVATAR_CHOICES_DICT
 
 
 class UserInfoSerializer(serializers.ModelSerializer):
-    AVATAR_CHOICES = {
-        0: "red_bust",
-        1: "blue_bust",
-        2: "green_bust",
-        3: "yellow_bust",
-        4: "pink_bust",
-    }
 
     avatar_file_name = serializers.SerializerMethodField()
 
@@ -18,5 +12,4 @@ class UserInfoSerializer(serializers.ModelSerializer):
         fields = ['nickname', 'avatar_file_name']
 
     def get_avatar_file_name(self, user):
-        avatar_num = user.avatar
-        return self.AVATAR_CHOICES.get(avatar_num)
+        return AVATAR_CHOICES_DICT.get(user.avatar)
