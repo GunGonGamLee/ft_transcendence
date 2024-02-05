dd88fc5fd7537c991c3d9d83084b1febd0bf5156
임원정 <wonlim@student.42seoul.kr>
2024-02-05 14:18:45 +0900

    feat: 닉네임 수정 api #173

--- backend/users/views.py
+++ backend/users/views.py
@@ -51,6 +51,26 @@ class SetNicknameView(APIView):
         except Exception as e:
             return JsonResponse({'error': e.__class__.__name__}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    def patch(self, request):
+        try:
+            user = AuthUtils.validate_jwt_token_and_get_user(request)
+            nickname = get_request_body_value(request, 'nickname')
+            user.nickname = nickname
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({'error': 'Token has expired'}, status=status.HTTP_401_UNAUTHORIZED)
+        except jwt.InvalidTokenError:
+            return JsonResponse({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+        except ValidationError as e:
+            return JsonResponse({'error': e.messages}, status=status.HTTP_401_UNAUTHORIZED)
+        except User.DoesNotExist:
+            return JsonResponse({'err_msg': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except IntegrityError:
+            return JsonResponse({'err_msg': 'duplicate nickname'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return JsonResponse({'error': e.__class__.__name__}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class UserInfoView(APIView):
     @swagger_auto_schema(
