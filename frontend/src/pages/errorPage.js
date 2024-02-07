991402cbbedb5d0a39389bedb7e9687c0c0f3f71
donghyun <donghyun1998@naver.com>
2024-02-08 00:29:21 +0900

    refactor: setState 실제 useState와 헷갈리므로 Init으로 변경

--- frontend/src/pages/errorPage.js
+++ frontend/src/pages/errorPage.js
@@ -1,38 +1,38 @@
-import { importCss } from '../utils/importCss.js';
+import { importCss } from "../utils/importCss.js";
 /**
  * @param {HTMLElement} $container
  * @param {number} errorCode
  */
 export default function ErrorPage($container, errorCode) {
   this.$container = $container;
-  this.comment = '';
+  this.comment = "";
 
-  this.setState = () => {
+  this.init = () => {
     switch (errorCode) {
       case 401:
-        this.comment = '어이 넌 권한이 없다.';
+        this.comment = "어이 넌 권한이 없다.";
         break;
       case 403:
-        this.comment = '클라이언트 요청 관련 오류.';
+        this.comment = "클라이언트 요청 관련 오류.";
         break;
       case 410:
-        this.comment = '사라진 리소스.';
+        this.comment = "사라진 리소스.";
         break;
       case 500:
-        this.comment = '알 수 없는 서버 내부 오류.';
+        this.comment = "알 수 없는 서버 내부 오류.";
         break;
       case 503:
-        this.comment = '일시적인 서버 오류.';
+        this.comment = "일시적인 서버 오류.";
         break;
       default:
-        this.comment = '알 수 없는 오류.';
+        this.comment = "알 수 없는 오류.";
         break;
     }
     this.render();
   };
 
   this.render = () => {
-    importCss('../../assets/css/errorPage.css');
+    importCss("../../assets/css/errorPage.css");
     this.$container.innerHTML = `
     <div class="errorPage-container">
       <span class="error-code">${errorCode} !<br/></span>
@@ -41,5 +41,5 @@ export default function ErrorPage($container, errorCode) {
     `;
   };
 
-  this.setState();
+  this.init();
 }
