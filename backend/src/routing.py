25c47f21d8dedc3f3f0c23b1dcac6fad14edeca6
sejoonkimmm <bod092512@gmail.com>
2024-02-08 15:07:13 +0900

    feat: src의 routing도 지웠다

--- backend/src/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
-from friends.consumers import ChatConsumer
-
-application = ProtocolTypeRouter({
-    "websocket": URLRouter([
-        path("ws/friend_status/", ChatConsumer),
-    ]),
-})
