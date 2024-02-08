898d0b178d74ec6273b30e930384b1bb9b62e733
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-08 17:11:25 +0900

    feat(header): 사용자 정보 받아오는 api 적용
    - 백엔드로부터 사용자 정보 받아와 헤더에 적용

--- frontend/src/header/mainHeader/header.js
+++ frontend/src/header/mainHeader/header.js
@@ -1,5 +1,7 @@
 import { hoverChangeCursor } from "../../utils/hoverEvent.js";
 import { importCss } from "../../utils/importCss.js";
+import { getCookie } from "../../utils/cookie.js";
+import { navigate } from "../../utils/navigate.js";
 
 /**
  * 사용자 전적 페이지에 사용하는 header 컴포넌트
@@ -10,22 +12,21 @@ export default function historiesHeader($container) {
   this.imagePath = "../../../assets/images";
 
   this.setState = () => {
-    const token = localStorage.getItem("jwtToken");
-    console.log("hi");
-    fetch("https://localhost/api/users/me", {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-        ...(token ? { Authorization: "Bearer " + token } : {}),
+    const token = getCookie("jwt");
+    fetch("https://localhost/api/users/me").then((response) => {
+      if (response.status === 200) {
+        response.json().then((data) => {
+          console.log(data);
+          this.render(data.nickname, data.avatar_file_name);
+        });
+      } else {
+        navigate("/500");
       }
-    });
-    document.getElementById("go-back").addEventListener("click", () => {
-      history.back();
     });
   };
 
   importCss("../../../assets/fonts/font.css");
-  this.render = () => {
+  this.render = (nickname, avatar_file_name) => {
     this.$container.innerHTML = `
         <div class="main header-wrapper">
             <div class="main" id="left-side">
@@ -33,15 +34,19 @@ export default function historiesHeader($container) {
             </div>
             <div class="main" id="title">사십 이 초-월</div>
             <div class="main" id="right-side">
-                <img src="${this.imagePath}/avatar/red_bust.png" alt="아바타" id="user-avatar">
+                <div class="main" id="user-info">
+                    <span class="main" id="nickname">${nickname}</span>
+                    <img src="${this.imagePath}/avatar/${avatar_file_name}" alt="아바타" id="user-avatar">
+                </div>
                 <img src="${this.imagePath}/friends.png" alt="친구 목록" id="friends">
             </div>
         </div>
         `;
 
-    hoverChangeCursor(document.getElementById("go-back"), "pointer");
-    hoverChangeCursor(document.getElementById("user-avatar"), "pointer");
-    hoverChangeCursor(document.getElementById("friends"), "pointer");
+    // 뒤로가기 버튼 클릭 이벤트
+    document.getElementById("go-back").addEventListener("click", () => {
+      history.back();
+    });
   };
 
   this.setState();
