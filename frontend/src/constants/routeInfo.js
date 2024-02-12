50dcffef5a1569cbcd03a8139f32f32a1964513a
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-12 15:16:56 +0900

    chore(frontend): 함수의 대소문자 정의와 호출이 일치하게 함

--- frontend/src/constants/routeInfo.js
+++ frontend/src/constants/routeInfo.js
@@ -1,22 +1,22 @@
-import GameMode from '../pages/game-mode/page.js';
-import auth from '../pages/auth/page.js';
-import Register from '../pages/register/page.js';
+import GameMode from "../pages/game-mode/page.js";
+import Register from "../pages/register/page.js";
 import Histories from "../pages/histories/page.js";
-import RegisterHeader from '../header/registerHeader/header.js';
-import MainHeader from '../header/mainHeader/header.js';
+import RegisterHeader from "../header/registerHeader/header.js";
+import MainHeader from "../header/mainHeader/header.js";
 import WaitingRoom from "../pages/waiting-room/page.js";
-import Login from '../pages/login/page.js'
-import CustomGameList from '../pages/custom-game-list/page.js';
+import Login from "../pages/login/page.js";
+import CustomGameList from "../pages/custom-game-list/page.js";
+import Auth from "../pages/auth/page.js";
 
 /**
  * 원하는 경로에 따라 렌더링할 컴포넌트를 정의합니다.
  */
 export const routes = [
   { path: /^\/$/, page: Login, header: RegisterHeader },
-  { path: /^\/auth(?:\?.*)?$/, page: auth, header: RegisterHeader },
+  { path: /^\/auth(?:\?.*)?$/, page: Auth, header: RegisterHeader },
   { path: /^\/register$/, page: Register, header: RegisterHeader },
   { path: /^\/game-mode$/, page: GameMode, header: MainHeader },
-  { path: /^\/histories$/, page: Histories, header: MainHeader},
+  { path: /^\/histories$/, page: Histories, header: MainHeader },
   { path: /^\/waiting-room$/, page: WaitingRoom, header: MainHeader },
-  { path: /^\/custom-game-list$/, page: CustomGameList, header: MainHeader}
-]
+  { path: /^\/custom-game-list$/, page: CustomGameList, header: MainHeader },
+];
