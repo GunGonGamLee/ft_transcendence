d31c51f3ac31fbdb815ae8aa3d840935e1692289
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-14 16:28:33 +0900

    refactor(histories): nav및 footer 리팩토링
    - nav에 불필요한 id 제거
    - renderLayout에서 nav와 footer를 렌더링하는 함수 분리

--- frontend/src/pages/histories/page.js
+++ frontend/src/pages/histories/page.js
@@ -19,6 +19,48 @@ export default function Histories($container) {
     this.renderList();
   };
 
+  this.renderNav = () => {
+    return `
+        <nav class="histories">
+            <div class="histories" id="summary">
+                <img class="histories" src="${HISTORIES_IMAGE_PATH}/custom_summary.png" alt="summary">
+                너의 기록은
+            </div>
+            <div class="histories" id="custom-menu-wrapper">
+                <div class="histories custom-toggle" id="custom">
+                    <img class="histories" src="${HISTORIES_IMAGE_PATH}/setting.png" alt="custom-mode">
+                    사용자 지정 모드
+                </div>
+                <div class="histories custom-toggle" id="toggle">
+                    <ul class="histories">
+                        <li>1 vs 1 모드</li>
+                        <li>토너먼트 모드</li>
+                    </ul>
+                </div>
+            </div>
+           <div class="histories title" id="tournament">
+               <img class="histories" src="${HISTORIES_IMAGE_PATH}/tournament_logo.png" alt="tournament">
+               토너먼트 모드
+           </div>
+        </nav>
+        `;
+  };
+
+  this.renderFooter = () => {
+    return `
+        <footer class="histories">
+            <div class="histories" id="search-wrapper">
+                <img class="histories" src="${HISTORIES_IMAGE_PATH}/search.png" alt="search">
+                <input class="histories" id="search" type="text" placeholder="유저 검색">
+            </div> 
+            <div class="histories" id="pagination">
+                <img src="${HISTORIES_IMAGE_PATH}/pagination.png" alt="prev" id="prev">
+                <img src="${HISTORIES_IMAGE_PATH}/pagination.png" alt="next" id="next">
+            </div>
+        </footer>
+        `;
+  };
+
   /**
    * 헤더, 그리고 개요-사용자 지정 모드-토너먼트 모드를 선택할 수 있는 버튼들을 렌더링합니다.
    * 이는 전적 리스트의 페이지의 레이아웃으로 추후 공통 모듈로 분리할 수 있습니다.
@@ -29,40 +71,10 @@ export default function Histories($container) {
       "afterbegin",
       `
         <div class="histories" id="content-wrapper">
-            <nav class="histories" id="mode">
-                <div class="histories" id="summary">
-                    <img class="histories" src="${HISTORIES_IMAGE_PATH}/custom_summary.png" alt="summary">
-                    너의 기록은
-                </div>
-                <div class="histories" id="custom-menu-wrapper">
-                    <div class="histories custom-toggle" id="custom">
-                        <img class="histories" src="${HISTORIES_IMAGE_PATH}/setting.png" alt="custom-mode">
-                        사용자 지정 모드
-                    </div>
-                    <div class="histories custom-toggle" id="toggle">
-                        <ul class="histories">
-                            <li>1 vs 1 모드</li>
-                            <li>토너먼트 모드</li>
-                        </ul>
-                    </div>
-                </div>
-               <div class="histories title" id="tournament">
-                   <img class="histories" src="${HISTORIES_IMAGE_PATH}/tournament_logo.png" alt="tournament">
-                   토너먼트 모드
-               </div>
-            </nav>
+            ${this.renderNav()}
             <div class="histories" id="content">
             </div>
-            <footer class="histories">
-                <div class="histories" id="search-wrapper">
-                    <img class="histories" src="${HISTORIES_IMAGE_PATH}/search.png" alt="search">
-                    <input class="histories" id="search" type="text" placeholder="유저 검색">
-                </div> 
-                <div class="histories" id="pagination">
-                    <img src="${HISTORIES_IMAGE_PATH}/pagination.png" alt="prev" id="prev">
-                    <img src="${HISTORIES_IMAGE_PATH}/pagination.png" alt="next" id="next">
-                </div>
-            </footer>
+            ${this.renderFooter()}
         </div>
         `,
     );
