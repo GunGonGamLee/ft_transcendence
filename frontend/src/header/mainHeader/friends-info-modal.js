43bdfcf399df23577fd1b353dc71b18a72ba9532
chj <bluedog129@gmail.com>
2024-02-15 21:40:19 +0900

    feat: 친구 정보 카드 세팅

--- frontend/src/header/mainHeader/friends-info-modal.js
+++ frontend/src/header/mainHeader/friends-info-modal.js
@@ -4,13 +4,51 @@ import {importCss} from '../../utils/importCss.js'
 export default function friendsInfoModal(isVisible) {
     importCss('../../../assets/css/friendsInfoModal.css');
 
+    const friends = [
+        {
+            username: 'hyojocho',
+            avatarImagePath: '../../assets/images/avatar/darth_vader.png',
+            iconImagePath: '../../assets/images/trash.png'
+        },
+        {
+            username: 'yena',
+            avatarImagePath: '../../assets/images/avatar/han_solo.png',
+            iconImagePath: '../../assets/images/trash.png'
+        },
+        // 배열은 최대 8개의 친구 정보 객체를 담을 수 있음
+        // ...
+    ];
+
+    function createFriendCard(friend) {
+        // friend 객체에서 필요한 정보를 직접 추출
+        const { username, avatarImagePath, iconImagePath } = friend;
+        return `
+        <div class="friend-card-wrapper">
+            <div class="avatar-image">
+                <image src="${avatarImagePath}"></image>
+            </div>
+            <div class="user-name">
+                ${username}
+            </div>
+            <div class="icon">
+                <image src="${iconImagePath}"></image>
+            </div>
+        </div>
+    `;
+    }
+
+    const friendCards = friends.slice(0, 8).map(friend =>
+        createFriendCard(friend)).join('');
+
     return `
         <div class="friends-info-modal-wrapper" id="friends-info-wrapper">
             <div class="list-wrapper" id="friends-list-wrapper">
                 <div class="list-subject">
-                    친구 (1 / 8)
+                    친구 (${friends.length} / 8)
                 </div>
-                <div id="friends-list"></div>
+                <div id="friends-list">
+                    ${friendCards}
+                </div>
             </div>
             <div class="list-wrapper" id="user-search-wrapper">
                 <div class="list-subject">
