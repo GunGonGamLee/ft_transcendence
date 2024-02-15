f42fb9325f34d71d36c72b3196ccc4cf27b571be
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-15 15:38:47 +0900

    refactor(histories): 전체 승률 삭제

--- frontend/src/pages/histories/summary-page.js
+++ frontend/src/pages/histories/summary-page.js
@@ -1,11 +1,11 @@
 export default async function Summary() {
-  this.$container = document.getElementById('content');
-  this.$pagination = document.getElementById('pagination');
+  this.$container = document.getElementById("content");
+  this.$pagination = document.getElementById("pagination");
   this.needToRender = false;
 
   this.init = () => {
-    this.$container.textContent = '';
-    this.$pagination.style.display = 'none';
+    this.$container.textContent = "";
+    this.$pagination.style.display = "none";
   };
 
   this.useState = async () => {
@@ -26,16 +26,23 @@ export default async function Summary() {
    */
   this.render = () => {
     if (!this.needToRender) return;
-    const { nickname, avatar, rating, win_rate, custom_win_rate, tournament_win_rate } = this.state;
+    const {
+      nickname,
+      avatar,
+      rating,
+      win_rate,
+      casual_win_rate,
+      tournament_win_rate,
+    } = this.state;
     this.$container.insertAdjacentHTML(
-      'afterbegin',
+      "afterbegin",
       `
     <div class="histories summary" id="summary-wrapper">
         <div class="histories summary" id="user-info">
             ${this.renderUserInfo({ nickname, avatar })}
         </div>
         <div class="histories summary" id="histories-info">
-            ${this.renderHistoriesSummary({ rating, win_rate, custom_win_rate, tournament_win_rate })}
+            ${this.renderHistoriesSummary({ rating, win_rate, casual_win_rate, tournament_win_rate })}
         </div>
     </div>
     `,
@@ -49,18 +56,18 @@ export default async function Summary() {
    *    avatar: string,
    *    rating: number,
    *    win_rate: number,
-   *    custom_win_rate: number,
+   *    casual_win_rate: number,
    *    tournament_win_rate: number
    * }>}
    */
   this.getUsersHistoriesSummary = async function () {
     // TODO => API 요청으로 await 해서 데이터 받아오기
     return {
-      nickname: 'yena',
-      avatar: '../../../assets/images/avatar/green.png',
+      nickname: "yena",
+      avatar: "../../../assets/images/avatar/green.png",
       rating: 2103,
       win_rate: 43,
-      custom_win_rate: 52,
+      casual_win_rate: 52,
       tournament_win_rate: 45,
     };
   };
@@ -69,7 +76,7 @@ export default async function Summary() {
    * 사용자의 정보를 렌더링합니다.
    * @param {{nickname: string, avatar: string}} props 사용자의 닉네임과 아바타 이미지 주소
    */
-  this.renderUserInfo = props => {
+  this.renderUserInfo = (props) => {
     return `
         <img class="histories summary" src="${props.avatar}" alt="avatar">
         <span>${props.nickname}</span>
@@ -78,22 +85,18 @@ export default async function Summary() {
 
   /**
    * 사용자의 전적 개요 데이터를 렌더링합니다.
-   * @param {{rating: number, win_rate: number, custom_win_rate: number, tournament_win_rate: number}} props 사용자의 전적 개요 데이터
+   * @param {{rating: number, casual_win_rate: number, tournament_win_rate: number}} props 사용자의 전적 개요 데이터
    */
-  this.renderHistoriesSummary = props => {
+  this.renderHistoriesSummary = (props) => {
     return `
       <div class="histories summary" id="data">
         <div class="histories summary" id="rating">
           <span>Rating: </span>
           <span>${props.rating}</span>
         </div>
-        <div class="histories summary" id="win-rate">
-          <span>전체 승률: </span>
-          <span>${props.win_rate}%</span>
-        </div>
-        <div class="histories summary" id="custom-win-rate">
-          <span>사용자 지정 모드 승률: </span>
-          <span>${props.custom_win_rate}%</span>
+        <div class="histories summary" id="casual-win-rate">
+          <span>캐주얼 모드 승률: </span>
+          <span>${props.casual_win_rate}%</span>
         </div>
         <div class="histories summary" id="tournament-win-rate">
           <span>토너먼트 모드 승률: </span>
