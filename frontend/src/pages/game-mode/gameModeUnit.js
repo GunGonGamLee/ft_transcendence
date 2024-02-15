54698b17dd21fa2f5168be9307e381e182a25707
donghyun <donghyun1998@naver.com>
2024-02-15 16:13:00 +0900

    feat: game-mode 페이지 스피너 ui 구현

--- frontend/src/pages/game-mode/gameModeUnit.js
+++ frontend/src/pages/game-mode/gameModeUnit.js
@@ -3,17 +3,32 @@
  * @param {number} unitNumber 유닛 번호
  */
 export default function GameModeUnit(unitNumber) {
-    const data = [
-        {img: "../../../assets/images/1vs1_logo.png", title: "2P 모드", description1: "한 컴퓨터에서 두 명의", description2: "사용자가 경기를 치른다."},
-        {img: "../../../assets/images/tournament_logo.png", title: "토너먼트 모드", description1: "랜덤으로 4명의 사용자와", description2: "토너먼트 경기를 치른다."},
-        {img: "../../../assets/images/setting.png", title: "사용자 지정 모드", description1: "다양한 모드로", description2: "게임을 즐길 수 있다."}
-    ];
-    return (`
-        <div class="game-mode-unit">
+  const data = [
+    {
+      img: "../../../assets/images/1vs1_logo.png",
+      title: "2P 모드",
+      description1: "한 컴퓨터에서 두 명의",
+      description2: "사용자가 경기를 치른다.",
+    },
+    {
+      img: "../../../assets/images/tournament_logo.png",
+      title: "토너먼트 모드",
+      description1: "랜덤으로 4명의 사용자와",
+      description2: "토너먼트 경기를 치른다.",
+    },
+    {
+      img: "../../../assets/images/setting.png",
+      title: "사용자 지정 모드",
+      description1: "다양한 모드로",
+      description2: "게임을 즐길 수 있다.",
+    },
+  ];
+  return `
+        <div class="game-mode-unit unit${unitNumber}">
             <img class="game-mode-image" src="${data[unitNumber].img}">
             <div class="game-mode-title">${data[unitNumber].title}</div>
             <div class="game-mode-description">${data[unitNumber].description1}</div>
             <div class="game-mode-description">${data[unitNumber].description2}</div>
         </div>
-    `);
+    `;
 }
