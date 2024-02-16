d3f873602d054bf6b337f3390b949cdcb9f2a413
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-16 18:18:54 +0900

    feat(header): 쿠키를 삭제하는 함수 추가

--- frontend/src/utils/cookie.js
+++ frontend/src/utils/cookie.js
@@ -14,3 +14,7 @@ export function getCookie(name) {
   }
   return null;
 }
+
+export function deleteCookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
