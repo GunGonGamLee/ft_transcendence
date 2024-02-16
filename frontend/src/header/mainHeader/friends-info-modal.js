51d59a252378c9d75497dbbdac8d3949196c3ee9
chj <bluedog129@gmail.com>
2024-02-16 20:17:49 +0900

    feat: 유저 infoCard 구현 완료

--- frontend/src/header/mainHeader/friends-info-modal.js
+++ frontend/src/header/mainHeader/friends-info-modal.js
@@ -4,52 +4,137 @@ import {importCss} from '../../utils/importCss.js'
 export default function friendsInfoModal(isVisible) {
     importCss('../../../assets/css/friendsInfoModal.css');
 
-    const friends = [
+    const friendList = [
         {
             username: '효조초다호호호호',
-            avatarImagePath: '../../assets/images/avatar/darth_vader.png',
-            iconImagePath: '../../assets/images/trash.png'
+            avatarImagePath: '../../assets/images/avatar/darth_vader.png'
         },
         {
             username: 'yena',
-            avatarImagePath: '../../assets/images/avatar/han_solo.png',
-            iconImagePath: '../../assets/images/trash.png'
+            avatarImagePath: '../../assets/images/avatar/han_solo.png'
         },
         {
             username: 'sejokim',
-            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png',
-            iconImagePath: '../../assets/images/trash.png'
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
         }
-        // 배열은 최대 8개의 친구 정보 객체를 담을 수 있음
-        // ...
     ];
 
-    function createFriendCard(friend) {
-        // friend 객체에서 필요한 정보를 직접 추출
+    const foundUserCardList = [
+        {
+            username: '효조초다호호호호',
+            avatarImagePath: '../../assets/images/avatar/darth_vader.png'
+        },
+        {
+            username: 'yena',
+            avatarImagePath: '../../assets/images/avatar/han_solo.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        }
+    ];
+
+    const requesterlist = [
+        {
+            username: '효조초다호호호호',
+            avatarImagePath: '../../assets/images/avatar/darth_vader.png'
+        },
+        {
+            username: 'yena',
+            avatarImagePath: '../../assets/images/avatar/han_solo.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        },
+        {
+            username: 'sejokim',
+            avatarImagePath: '../../assets/images/avatar/luke_skywalker.png'
+        }
+    ];
+
+    function createInfoCard(friend, style = {}, image = {}) {
         const { username, avatarImagePath, iconImagePath } = friend;
+        const borderColor = style.borderColor;
+        const imagePath = image.iconImagePath;
+        // accept.png 아이콘일 경우에만 추가할 HTML 조각을 정의
+        const additionalIconHTML = imagePath === '../../assets/images/accept.png' ?
+            `<div>
+            <img class="icon" src="../../assets/images/close.png" />
+        </div>` : '';
+        // 조건에 따라 클래스 추가
+        const wrapperClass = imagePath === '../../assets/images/accept.png' ? 'friend-card-wrapper with-additional-icon' : 'friend-card-wrapper';
+
         return `
-        <div class="friend-card-wrapper">
+        <div class="${wrapperClass}" style="border-color: ${borderColor};">
             <div>
-                <image class="avatar-image" src="${avatarImagePath}"></image>
+                <img class="avatar-image" src="${avatarImagePath}" />
             </div>
             <div class="user-name">
                 ${username}
             </div>
-            <div >
-                <image class="icon" src="${iconImagePath}"></image>
+            <div>
+                <img class="icon" src="${imagePath}" />
             </div>
+            ${additionalIconHTML}
         </div>
     `;
     }
 
-    const friendCards = friends.slice(0, 8).map(friend =>
-        createFriendCard(friend)).join('');
+    const friendCards = friendList.slice(0, 8).map(card =>
+        createInfoCard(card, {borderColor: '#07F7B0'}, {iconImagePath: '../../assets/images/trash.png'})).join('');
+
+    const foundUserCards = foundUserCardList.map((card) =>
+        createInfoCard(card, {borderColor: '#FF52A0'}, {iconImagePath: '../../assets/images/paper_plane.png'})).join('');
+
+    const requesterCards = requesterlist.map(card =>
+        createInfoCard(card, {borderColor: '#29ABE2'}, {iconImagePath: '../../assets/images/accept.png'})).join('');
 
     return `
         <div class="friends-info-modal-wrapper" id="friends-info-wrapper">
             <div class="list-wrapper" id="friends-list-wrapper">
                 <div class="list-subject">
-                    친구 (${friends.length} / 8)
+                    친구 (${friendList.length} / 8)
                 </div>
                 <div id="friends-list">
                     ${friendCards}
@@ -57,19 +142,23 @@ export default function friendsInfoModal(isVisible) {
             </div>
             <div class="list-wrapper" id="user-search-wrapper">
                 <div class="list-subject">
-                    유저 찾기
+                    유저 찾기 (${foundUserCardList.length})
                 </div>
                 <div id="search-form">
                     <image src="../../assets/images/search.png"></image>
                     <input />
                 </div>
-                <div id="user-search"></div>
+                <div id="user-search">
+                    ${foundUserCards}
+                </div>
             </div>
             <div class="list-wrapper" id="friend-request-list-wrapper">
                 <div class="list-subject">
-                    친구 요청
+                    친구 요청 (${requesterlist.length})
                 </div>
-                <div id="friend-request-list"></div>
+                <div id="friend-request-list">
+                    ${requesterCards}
+                </div>
             </div>
         </div>
     `;
