40ba14b39323294db97c33f7855fdba8cf6d477a
donghyun <donghyun1998@naver.com>
2024-02-16 14:05:07 +0900

    refactor: 고정픽셀 다 뷰포트로 변경

--- frontend/src/pages/in-game/page.js
+++ frontend/src/pages/in-game/page.js
@@ -2,57 +2,63 @@ import useState from "../../utils/useState.js";
 import scoreBar from "./scoreBar.js";
 
 export default function InGame($container) {
-
-	const scoreInput = {player1: 0, player2: 0};
-	let [getScore, setScore] = useState(scoreInput, this, 'renderScoreBoard');
-	const init = () => {
-		hideHeader();
-		this.render();
-		this.renderScoreBoard();
-		addKeyEvents();
-	}
-	// TODO: 다른 페이지로 이동 시 이벤트 제거, mainheader 보이게 수정
-	this.render = () => {
-		$container.innerHTML = `
+  const scoreInput = { player1: 0, player2: 0 };
+  let [getScore, setScore] = useState(scoreInput, this, "renderScoreBoard");
+  const init = () => {
+    hideHeader();
+    this.render();
+    this.renderScoreBoard();
+    addKeyEvents();
+  };
+  // TODO: 다른 페이지로 이동 시 이벤트 제거, mainheader 보이게 수정
+  this.render = () => {
+    $container.innerHTML = `
 			${scoreBar()}
 			<div class="in-game" style="height: 100vh; width: 100vw; background-image: url('../../../assets/images/ingame_background.png'); background-size: cover">
 			<div class="bar1" style="position: fixed; top: 40vh; left: 10vw; height: 20vh; width: 1vw; background-image: url('../../../assets/images/light_saber_green.png'); background-size: cover"></div>
 			<div class="bar2" style="position: fixed; top: 40vh; left: 90vw; height: 20vh; width: 1vw; background-image: url('../../../assets/images/light_saber_yellow.png'); background-size: cover;"></div>
 			<div class="ball" style="position: fixed; top: 48vh; left: 48vw; height: 4vh; width: 4vh; background-image: url('../../../assets/images/storm_trooper.png'); background-size: cover;"></div>
-			`
-	}
-	this.renderScoreBoard = () => {
-		$container.querySelector(".score-board").innerHTML = `
-				<span style="color: yellow; font-size: 2em; margin: auto;">${getScore().player1} 대 ${getScore().player2}</span>
-		`
-	}
-	const hideHeader = () => {
-		document.querySelector("#header").style.display = "none";
-	}
-	const addKeyEvents = () => {
-		// TODO: window에 추가한 이벤트라 리소스 누수가 발생할 수 있음 (컴포넌트 제거 시 이벤트 제거 필요)
-		window.addEventListener("keydown", (e) => {
-			if (e.key === "w" || e.key === "W" || e.key === 'ㅈ') {
-				const currentTop = parseInt($container.querySelector(".bar1").style.top);
-				if (currentTop <= 12) return;
-				$container.querySelector(".bar1").style.top = `${currentTop - 1}vh`;
-			}
-			else if (e.key === "s" || e.key === "S" || e.key === 'ㄴ') {
-				const currentTop = parseInt($container.querySelector(".bar1").style.top);
-				if (currentTop >= 80) return;
-				$container.querySelector(".bar1").style.top = `${currentTop + 1}vh`;
-			} // TODO: 2p 아닐땐 아래 이벤트리스너 실행 안되게 수정
-			else if (e.key === "ArrowUp") {
-				const currentTop = parseInt($container.querySelector(".bar2").style.top);
-				if (currentTop <= 12) return;
-				$container.querySelector(".bar2").style.top = `${currentTop - 1}vh`;
-			} else if (e.key === "ArrowDown") {
-				const currentTop = parseInt($container.querySelector(".bar2").style.top);
-				if (currentTop >= 80) return;
-				$container.querySelector(".bar2").style.top = `${currentTop + 1}vh`;
-			}
-		})
-	}
+			`;
+  };
+  this.renderScoreBoard = () => {
+    $container.querySelector(".score-board").innerHTML = `
+				<span style="color: yellow; font-size: 6vh; margin: auto;">${getScore().player1} 대 ${getScore().player2}</span>
+		`;
+  };
+  const hideHeader = () => {
+    document.querySelector("#header").style.display = "none";
+  };
+  const addKeyEvents = () => {
+    // TODO: window에 추가한 이벤트라 리소스 누수가 발생할 수 있음 (컴포넌트 제거 시 이벤트 제거 필요)
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "w" || e.key === "W" || e.key === "ㅈ") {
+        const currentTop = parseInt(
+          $container.querySelector(".bar1").style.top,
+        );
+        if (currentTop <= 12) return;
+        $container.querySelector(".bar1").style.top = `${currentTop - 1}vh`;
+      } else if (e.key === "s" || e.key === "S" || e.key === "ㄴ") {
+        const currentTop = parseInt(
+          $container.querySelector(".bar1").style.top,
+        );
+        if (currentTop >= 80) return;
+        $container.querySelector(".bar1").style.top = `${currentTop + 1}vh`;
+      } // TODO: 2p 아닐땐 아래 이벤트리스너 실행 안되게 수정
+      else if (e.key === "ArrowUp") {
+        const currentTop = parseInt(
+          $container.querySelector(".bar2").style.top,
+        );
+        if (currentTop <= 12) return;
+        $container.querySelector(".bar2").style.top = `${currentTop - 1}vh`;
+      } else if (e.key === "ArrowDown") {
+        const currentTop = parseInt(
+          $container.querySelector(".bar2").style.top,
+        );
+        if (currentTop >= 80) return;
+        $container.querySelector(".bar2").style.top = `${currentTop + 1}vh`;
+      }
+    });
+  };
 
-	init();
+  init();
 }
--- frontend/src/pages/in-game/scoreBar.js
+++ frontend/src/pages/in-game/scoreBar.js
@@ -1,14 +1,15 @@
 export default function scoreBar() {
-	return `
+  return `
 		<div style="position: fixed; top: 0; left:0; height: 12vh; width:100vw; background-color: black; display: flex; font-family: Galmuri11-Bold, serif; justify-content: space-between; align-items: center">
 				<div class="left-unit" style="width: 30vw; height: 12vh">
 				<img alt="loading" src="../../../assets/images/avatar/luke_skywalker.png" style="height: 100%; margin-left: 5vw;">
 				</div>
 				<div class="score-board"></div>
 				<div class="right-unit" style="width: 30vw; height: 12vh; display: flex; flex-direction: row; align-items: center; justify-content: space-between">	
-						<span style="color: white; font-size: 1.5em; font-family: Galmuri11, serif"><img alt="loading" src="../../../assets/images/clock.png" style="height: 10vh%; "> 00:00</span>					
+						<img alt="loading" src="../../../assets/images/clock.png" style="height: 4vh"> 
+						<span style="color: white; font-size: 3vh; font-family: Galmuri11, serif; margin-right: 10vh">00:00</span>					
 					<img alt="loading" src="../../../assets/images/avatar/darth_vader.png" style="height: 100%; margin-right: 5vw;">
 				</div>
 			</div>
-	`
+	`;
 }
