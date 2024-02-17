cf41370cc2fd96431dadf09dda6a35c4d0216975
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-18 07:00:55 +0900

    feat(users): url name 변경

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('nickname/', SetNicknameView.as_view(), name='nickname'),
     path('me/', UserMeInfoView.as_view(), name='userMeInfo'),
     path('<str:nickname>/', UserInfoView.as_view(), name='userInfo'),
-    path('<str:nickname>/avatar/', UserAvatarView.as_view(), name='avatarUpdate'),
+    path('<str:nickname>/avatar/', UserAvatarView.as_view(), name='userAvatar'),
 ]
