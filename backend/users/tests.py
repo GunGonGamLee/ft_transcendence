7de9f864b690a662f147fbeb3952228c34a17f3f
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-17 13:51:28 +0900

    test(users): 아바타 업데이트 관련 테스트 프로토타입 정의

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+class AvatarUpdateTest(TestCase):
+    def test_avatar_upload(self):
+        pass
+
+    def test_avatar_upload_duplicate(self):
+        pass
+
+    def test_avatar_upload_fail(self):
+        pass
+
+    def test_avatar_upload_invalid(self):
+        pass
+
+    def test_avatar_upload_unauthorized(self):
+        pass
+
+    def test_avatar_upload_server_error(self):
+        pass
+
+    def test_avatar_update(self):
+        pass
