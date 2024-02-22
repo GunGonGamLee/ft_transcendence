dc6676884719fe2ac3296b78be6f644cf84ae304
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-22 17:25:08 +0900

    feat(runGame): moveBall() 함수 프로토타입 추가

--- frontend/src/pages/in-game/runGame.js
+++ frontend/src/pages/in-game/runGame.js
@@ -1,16 +1,17 @@
-export const runGame = (canvas, bar1, bar2, ball) => {
-  const init = () => {
-    let ball_direction = {
-      x: Math.round(Math.random()) * 2 - 1,
-      y: Math.round(Math.random()) * 2 - 1,
-    };
-    this.canvas = canvas;
-    this.bar1 = bar1;
-    this.bar2 = bar2;
-    this.ball = {
-      ...ball,
-      speed: 10,
-      direction: ball_direction,
-    };
+/**
+ * 게임 실행 함수
+ * @param canvas {HTMLElement} 게임이 실행될 캔버스
+ * @param bar1 {object} 바의 x, y, width, height, speed
+ * @param bar2 {object} 바의 x, y, width, height, speed
+ * @param ball {object} 공의 x, y, radius, direction, speed
+ * @param drawFunction {function} 캔버스를 그리는 함수
+ */
+export const runGame = (canvas, bar1, bar2, ball, drawFunction) => {
+  const moveBall = () => {
+    ball.x += ball.direction.x * ball.speed;
+    ball.y += ball.direction.y * ball.speed;
+    if (does_ball_hit_wall(canvas, ball)) {
+      ball.direction.y *= -1;
+    }
   };
 };
