5037e649e957458697d0cf27bf3ef4623a4e3968
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-22 17:30:59 +0900

    feat(runGame): 공이 벽에 부딪혔는지 확인하는 함수 구현

--- frontend/src/pages/in-game/runGame.js
+++ frontend/src/pages/in-game/runGame.js
@@ -10,8 +10,22 @@ export const runGame = (canvas, bar1, bar2, ball, drawFunction) => {
   const moveBall = () => {
     ball.x += ball.direction.x * ball.speed;
     ball.y += ball.direction.y * ball.speed;
-    if (does_ball_hit_wall(canvas, ball)) {
+    if (isBallHitWall(canvas, ball)) {
       ball.direction.y *= -1;
     }
   };
+
+  /**
+   * 공이 벽에 부딪혔는지 확인하는 함수
+   * @param canvas {HTMLElement} 게임이 실행될 캔버스
+   * @param ball {object} 공의 x, y, radius, direction, speed
+   */
+  const isBallHitWall = (canvas, ball) => {
+    let topPoint = ball.y - ball.radius;
+    let bottomPoint = ball.y + ball.radius;
+
+    return topPoint <= 0 || bottomPoint >= canvas.offsetHeight;
+  };
+
+  moveBall();
 };
