04e137ad2db0cc7756bccf9345c4caee316e74a3
chj <bluedog129@gmail.com>
2024-02-27 14:20:58 +0900

    feat: 친구 추가, 수락, 삭제 이벤트 이후 스피너 갱신

--- frontend/src/header/mainHeader/header.js
+++ frontend/src/header/mainHeader/header.js
@@ -271,15 +271,14 @@ export default function MainHeader($container) {
                         if (response.status === 200) {
                             const loadingHtml = `
                                 <div style="position: relative; width: 100%; height: 100%;">
-                                    <div id="loading-spinner" style="position: fixed; top: 5vh; bottom: 5vh;">               
+                                    <div id="loading-spinner" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">               
                                         <div class="spinner-border text-light" style="width: 10vh; height: 10vh;" role="status">
-                                            <span class="visually-hidden">Loading...</span>
                                         </div>
                                     </div>
                                 </div>
                             `;
                             const friendsListDiv = document.getElementById('friends-list');
-                            friendsListDiv.innerHTML += loadingHtml; // 로딩 스피너를 friends-list 내부에 추가
+                            friendsListDiv.innerHTML = loadingHtml; // 로딩 스피너를 friends-list 내부에 갱신
                         } else {
                             // TODO => 에러 페이지로 이동
                             navigate("/");
@@ -288,10 +287,23 @@ export default function MainHeader($container) {
                 });
 
             }
+
+
+        });
+        click(document.getElementById("friends-list"), () => {
+            const loadingHtml = `
+                                <div style="position: relative; width: 100%; height: 100%;">
+                                    <div id="loading-spinner" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">               
+                                        <div class="spinner-border text-light" style="width: 10vh; height: 10vh;" role="status">
+                                        </div>
+                                    </div>
+                                </div>
+                            `;
+            const friendsListDiv = document.getElementById('friends-list');
+            friendsListDiv.innerHTML = loadingHtml; // 로딩 스피너를 friends-list 내부에 갱신
         });
     };
 
-    // 친구요청 받은 리스트 렌더링
     // 친구요청 받은 리스트 렌더링 함수
     this.renderRequestersList = () => {
         const newRequestersList = getRequestersList();
@@ -319,7 +331,18 @@ export default function MainHeader($container) {
                 },
                 body: JSON.stringify({nickname})
             }).then(response => {
-                if (response.status !== 200) {
+                if (response.status === 200) {
+                    const loadingHtml = `
+                                <div style="position: relative; width: 100%; height: 100%;">
+                                    <div id="loading-spinner" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                                        <div class="spinner-border text-light" style="width: 10vh; height: 10vh;" role="status">
+                                        </div>
+                                    </div>
+                                </div>
+                            `;
+                    const friendsListDiv = document.getElementById('friend-request-list');
+                    friendsListDiv.innerHTML = loadingHtml; // 로딩 스피너를 friend-request-list 내부에 갱신
+                } else {
                     navigate("/");
                 }
                 // 성공적으로 처리됐을 때의 로직 (예: 리스트 갱신, 알림 등)
@@ -335,31 +358,9 @@ export default function MainHeader($container) {
 
             if (acceptIcon) {
                 acceptIcon.addEventListener('click', () => handleFriendRequest('accept', requester.nickname));
-                const loadingHtml = `
-                                <div style="position: relative; width: 100%; height: 100%;">
-                                    <div id="loading-spinner" style="position: fixed; top: 5vh; bottom: 5vh;">               
-                                        <div class="spinner-border text-light" style="width: 10vh; height: 10vh;" role="status">
-                                            <span class="visually-hidden">Loading...</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            `;
-                const friendsListDiv = document.getElementById('friends-list');
-                friendsListDiv.innerHTML += loadingHtml; // 로딩 스피너를 friends-list 내부에 추가
             }
             if (rejectIcon) {
                 rejectIcon.addEventListener('click', () => handleFriendRequest('reject', requester.nickname));
-                const loadingHtml = `
-                                <div style="position: relative; width: 100%; height: 100%;">
-                                    <div id="loading-spinner" style="position: fixed; top: 5vh; bottom: 5vh;">               
-                                        <div class="spinner-border text-light" style="width: 10vh; height: 10vh;" role="status">
-                                            <span class="visually-hidden">Loading...</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            `;
-                const friendsListDiv = document.getElementById('friends-list');
-                friendsListDiv.innerHTML += loadingHtml; // 로딩 스피너를 friends-list 내부에 추가
             }
         });
     };
