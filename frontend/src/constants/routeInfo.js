11b225dece03708f53e879821204c4015f6a50a4
donghyun kim <donghyun1998@naver.com>
2024-02-28 14:51:53 +0900

    feat: errorpage 헤더 수정 game mode 소켓 연결 후 navigate waiting room에서 tournament로 소켓연결후 보내게 변경 (#357)
    * fix: be 함수명 변경(뭔지모름)
    
    * feat: game-mode socket연결해서 보내줌
    
    * chore: 임시로 중복로그인 해제해둠
    
    * feat: tournament 페이지에 socket 보내서 변수로 달아줌
    
    * feat: waiting-room socket연결해서 tournament로 보내줌
    
    * refactor: empty-header로 이름 변경

--- frontend/src/constants/routeInfo.js
+++ frontend/src/constants/routeInfo.js
@@ -1,7 +1,7 @@
 import GameMode from "../pages/game-mode/page.js";
 import Register from "../pages/register/page.js";
 import Histories from "../pages/histories/page.js";
-import RegisterHeader from "../header/registerHeader/header.js";
+import EmptyHeader from "../header/emptyHeader/header.js";
 import MainHeader from "../header/mainHeader/header.js";
 import WaitingRoom from "../pages/waiting-room/page.js";
 import Login from "../pages/login/page.js";
@@ -14,9 +14,9 @@ import Tournament from "../pages/tournament/page.js";
  * 원하는 경로에 따라 렌더링할 컴포넌트를 정의합니다.
  */
 export const routes = [
-  { path: /^\/$/, page: Login, header: RegisterHeader },
-  { path: /^\/auth(?:\?.*)?$/, page: Auth, header: RegisterHeader },
-  { path: /^\/register$/, page: Register, header: RegisterHeader },
+  { path: /^\/$/, page: Login, header: EmptyHeader },
+  { path: /^\/auth(?:\?.*)?$/, page: Auth, header: EmptyHeader },
+  { path: /^\/register$/, page: Register, header: EmptyHeader },
   { path: /^\/game-mode$/, page: GameMode, header: MainHeader },
   { path: /^\/histories$/, page: Histories, header: MainHeader },
   { path: /^\/waiting-room$/, page: WaitingRoom, header: MainHeader },
