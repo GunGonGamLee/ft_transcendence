1133b2c9fbc5f6d07c36d885d596e77fb8d57665
donghyun <donghyun1998@naver.com>
2024-02-28 15:39:55 +0900

    feat: info 제대로 왔는지 console코드 추가

--- frontend/src/pages/tournament/page.js
+++ frontend/src/pages/tournament/page.js
@@ -2,16 +2,17 @@ export default function Tournament($container, info = null) {
   if (info === null) {
     return;
   }
-
+  console.log(info);
   const ws = info.socket;
+  ws.onmessage = (msg) => {
+    let data = JSON.parse(msg.data);
+    console.log(data);
+  };
 
   const init = () => {
     render();
   };
 
-  this.unmount = () => {
-    // ws.close();
-  };
   const render = () => {
     $container.innerHTML = `
 			<div class="tournament-wrapper" style="background-image: url('../../../assets/images/tournament_background.png'); background-size: 100% 100%; background-repeat: no-repeat; width: 100vw; height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center">
