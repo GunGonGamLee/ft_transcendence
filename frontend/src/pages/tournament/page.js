43539f67cc953bf85d4aafc11e00c5652f4759d0
chj <bluedog129@gmail.com>
2024-02-28 16:06:20 +0900

    feat: 토너먼트 4강 html 구성완료

--- frontend/src/pages/tournament/page.js
+++ frontend/src/pages/tournament/page.js
@@ -1,3 +1,5 @@
+import {importCss} from "../../utils/importCss.js";
+
 export default function Tournament($container, info = null) {
   // const gameModeNum = info.data.mode;
 
@@ -12,8 +14,70 @@ export default function Tournament($container, info = null) {
     // ws.close();
   };
   const render = () => {
+    importCss("../../../assets/css/tournament.css");
+
     $container.innerHTML = `
-			<div class="tournament-wrapper" style="background-image: url('../../../assets/images/tournament_background.png'); background-size: 100% 100%; background-repeat: no-repeat; width: 100vw; height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center">
+        <div class="background-wrapper">
+        </div>
+        <div class="tournament-wrapper">
+            <div class="left-tournament-info-wrapper">
+                <div class="card-wrapper">
+                    <div class="user-avatar">
+                        <img src="../../../assets/images/avatar/darth_vader.png">
+                    </div>
+                    <div class="user-name">hyojocho</div>
+                    <div class="user-rating">Rating: 1,000</div>
+                </div>
+                <div class="card-wrapper">
+                    <div class="user-avatar">
+                        <img src="../../../assets/images/avatar/darth_vader.png">
+                    </div>
+                    <div class="user-name">hyojocho</div>
+                    <div class="user-rating">Rating: 1,000</div>
+                </div>
+            </div>
+            <div class="first-middle-tournament-info-wrapper">
+                <div class="top-line-wrapper">
+                </div>
+                <div class="middle-line-wrapper">
+                </div>
+                <div class="bottom-line-wrapper">
+                </div>
+            </div>
+            <div class="second-middle-tournament-info-wrapper">
+                <div class="second-side-wrapper">
+                </div>
+                <div id="trophy-wrapper">
+                    <img src="../../../assets/images/tournament_logo.png">
+                </div>
+                <div class="second-side-wrapper">
+                </div>
+            </div>
+            <div class="third-tournament-info-wrapper">
+                <div class="top-line-wrapper">
+                </div>
+                <div class="middle-line-wrapper">
+                </div>
+                <div class="bottom-line-wrapper">
+                </div>
+            </div>
+            <div class="right-tournament-info-wrapper">
+                <div class="card-wrapper">
+                    <div class="user-avatar">
+                        <img src="../../../assets/images/avatar/darth_vader.png">
+                    </div>
+                    <div class="user-name">hyojocho</div>
+                    <div class="user-rating">Rating: 1,000</div>
+                </div>
+                <div class="card-wrapper">
+                    <div class="user-avatar">
+                        <img src="../../../assets/images/avatar/darth_vader.png">
+                    </div>
+                    <div class="user-name">hyojocho</div>
+                    <div class="user-rating">Rating: 1,000</div>
+                </div>
+            </div>
+        </div>
 		`;
   };
   init();
