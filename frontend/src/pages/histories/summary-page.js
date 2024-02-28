90d4b0c010a791ce061cdeb4cc2c5faf55c6befa
yena <50291995+nyj001012@users.noreply.github.com>
2024-02-28 13:36:12 +0900

    feat(histories): 개요 페이지 백엔드 API 이식 및 useState 적용 완료

--- frontend/src/pages/histories/summary-page.js
+++ frontend/src/pages/histories/summary-page.js
@@ -1,38 +1,42 @@
-import { HISTORIES_IMAGE_PATH, MODE } from "../../global.js";
-import { useState } from "../../state.js";
+import { BACKEND, HISTORIES_IMAGE_PATH, MODE } from "../../global.js";
+import { getCookie } from "../../utils/cookie.js";
+import useState from "../../utils/useState.js";
 
 export default function Summary() {
   this.$container = document.getElementById("content");
   this.$pagination = document.getElementById("pagination");
-  let [getUsersHistoriesSummary, setUsersHistoriesSummary] = useState(
-    {
-      nickname: "yena",
-      avatar: "chewbacca.png",
-      rating: 2103,
-      win_rate: 43,
-      casual_win_rate: 52,
-      tournament_win_rate: 45,
-    },
-    "render",
-  );
 
   const init = () => {
     this.$container.textContent = "";
     this.$pagination.style.display = "none";
+    let nickname = "예나";
+    fetch(`${BACKEND}/users/${nickname}/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getCookie("jwt")}`,
+      },
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          setUsersHistoriesSummary(data);
+        });
+      }
+    });
   };
 
   /**
    * 사용자의 전적 개요를 렌더링합니다.
    */
-  const render = () => {
+  this.render = () => {
     const {
       nickname,
       avatar,
       rating,
-      win_rate,
-      casual_win_rate,
-      tournament_win_rate,
-    } = this.state;
+      custom_1v1_win_rate,
+      custom_tournament_win_rate,
+      rank_win_rate,
+    } = getUsersHistoriesSummary();
     this.$container.insertAdjacentHTML(
       "afterbegin",
       `
@@ -41,7 +45,7 @@ export default function Summary() {
             ${renderUserInfo({ nickname, avatar })}
         </div>
         <div class="histories summary" id="data">
-            ${renderHistoriesSummary({ rating, win_rate, casual_win_rate, tournament_win_rate })}
+            ${renderHistoriesSummary({ rating, custom_1v1_win_rate, custom_tournament_win_rate, rank_win_rate })}
         </div>
     </div>
     `,
@@ -61,7 +65,12 @@ export default function Summary() {
 
   /**
    * 사용자의 전적 개요 데이터를 렌더링합니다.
-   * @param {{rating: number, casual_win_rate: number, tournament_win_rate: number}} props 사용자의 전적 개요 데이터
+   * @param {{
+   * rating: number,
+   * custom_1v1_win_rate: number,
+   * custom_tournament_win_rate: number,
+   * rank_win_rate: number
+   * }} props 사용자의 전적 개요 데이터
    */
   const renderHistoriesSummary = (props) => {
     return `
@@ -71,17 +80,26 @@ export default function Summary() {
       </div>
       <div class="histories summary" id="casual-win-rate">
         <span>${MODE.casual} 승률: </span>
-        <span>${props.casual_win_rate}%</span>
+        <span>${props.custom_1v1_win_rate}%</span>
       </div>
       <div class="histories summary" id="tournament-win-rate">
         <span>${MODE.tournament} 승률: </span>
-        <span>${props.tournament_win_rate}%</span>
+        <span>${props.rank_win_rate}%</span>
       </div>
     `;
   };
 
   init();
-  useState();
-  setState();
-  render();
+  const [getUsersHistoriesSummary, setUsersHistoriesSummary] = useState(
+    {
+      nickname: "",
+      avatar: "",
+      rating: 0,
+      custom_1v1_win_rate: 0,
+      custom_tournament_win_rate: 0,
+      rank_win_rate: 0,
+    },
+    this.$container,
+    "render",
+  );
 }
