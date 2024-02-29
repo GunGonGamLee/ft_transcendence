f220e61681e8ffbcc6f96b2b6bdb72912476e490
임원정 <wonlim@student.42seoul.kr>
2024-02-29 16:00:32 +0900

    feat: add local url

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
 	path('', GameView.as_view(), name='game'),
+	path('local/', LocalGameView.as_view(), name='localGame'),
 	path('<str:game_id>/', GameRoomView.as_view(), name='gameRoom'),
 	path('results', GameResultListView.as_view(), name='gameResultList'),
 	path('results/<str:game_id>', GameResultView.as_view(), name='gameResult'),
