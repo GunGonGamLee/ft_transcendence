926750d020987fc647dca18d8d5b943efb8383f8
donghyun <donghyun1998@naver.com>
2024-03-04 16:22:49 +0900

    feat: waiting-room 인원 검사 로직 추가

--- frontend/src/pages/waiting-room/page.js
+++ frontend/src/pages/waiting-room/page.js
@@ -17,13 +17,14 @@ export default function WaitingRoom($container, info = null) {
     return;
   }
   let userNickname = null;
+  let players = info.data.players;
+
   getUserMe().then((user) => {
     userNickname = user.data.nickname;
   });
 
   const gameModeNum = info.data.mode;
   const ws = info.socket;
-  let props = info.data.players;
 
   const init = () => {
     render();
@@ -42,7 +43,8 @@ export default function WaitingRoom($container, info = null) {
     ws.onmessage = (msg) => {
       let data = JSON.parse(msg.data);
       if (data.type === "game_info") {
-        setUserState(data.data.players);
+        players = data.data.players;
+        setUserState(players);
       } else {
         console.log(data);
         const newWs = new WebSocket(`${WEBSOCKET}${data.data}`);
@@ -54,13 +56,19 @@ export default function WaitingRoom($container, info = null) {
     };
 
     click($container.querySelector(".start-btn"), () => {
-      // TODO: 인원 다 차야 시작 가능
-
       // 방장만 게임 시작 가능
       const player = info.data.players.find(
         (player) => player.nickname === userNickname,
       );
-      if (player && !player.is_manager) return;
+      if (player && !player.is_manager) {
+        alert("방장만 게임을 시작할 수 있습니다.");
+        return;
+      }
+      // 인원 다 차야 시작 가능
+      if (players.length !== 4) {
+        alert("인원이 다 차야 게임을 시작할 수 있습니다.");
+        return;
+      }
       ws.send(
         JSON.stringify({
           type: "game_start",
@@ -85,7 +93,7 @@ export default function WaitingRoom($container, info = null) {
           <span style="background-color: black; padding: 0.4vh;">${info.password}</span>
         </div>
         <div class="user-box-wrapper" style="width: 100vw; height: 65vh; display : flex; flex-direction: row">
-          ${userBox(gameModeNum, props)}
+          ${userBox(gameModeNum, players)}
         </div>
         <div class="start-btn-wrapper" style="width: 100vw; height: 10vh; display: flex; justify-content: center; align-items: center">
           <button class="start-btn" style="background: linear-gradient(to bottom, #D80000, #FF0000); font-family: Galmuri11-Bold, serif; color: white; border: 4px solid darkred; padding: 10px 40px; text-align: center; text-decoration: none; display: inline-block; font-size: 32px; margin: 4px 2px; cursor: pointer; border-radius: 5vh;">START</button>
@@ -101,5 +109,5 @@ export default function WaitingRoom($container, info = null) {
   };
 
   init();
-  let [getUserState, setUserState] = useState(props, this, "renderUserBox");
+  let [getUserState, setUserState] = useState(players, this, "renderUserBox");
 }
