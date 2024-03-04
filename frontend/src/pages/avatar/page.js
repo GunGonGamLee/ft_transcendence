ed947705c338977f1f3f813c338a7c3d04d9f401
yena <50291995+nyj001012@users.noreply.github.com>
2024-03-04 17:55:43 +0900

    feat(avatar): 아바타 페이지 렌더링 구현

--- frontend/src/pages/avatar/page.js
+++ frontend/src/pages/avatar/page.js
@@ -1,11 +1,50 @@
 import { Histories } from "../histories/page.js";
+import useState from "../../utils/useState.js";
+import { AVATAR_FILE_NAME, HISTORIES_IMAGE_PATH } from "../../global.js";
 
 export default function Avatar() {
   new Histories(document.getElementById("app"));
+  this.$content = document.getElementById("content");
   this.$pagination = document.getElementById("pagination");
+
   const init = () => {
     this.$pagination.style.display = "none";
   };
 
+  const renderAvatarExamples = () => {
+    let html = "";
+    for (let avatar of AVATAR_FILE_NAME) {
+      html += `
+        <img class="avatar examples" src="${HISTORIES_IMAGE_PATH}/avatar/${avatar}" alt="${avatar}">
+      `;
+    }
+    return html;
+  };
+
+  const render = () => {
+    this.$content.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="avatar-wrapper basic">
+        <div class="avatar update-type">
+            기본
+        </div>
+        <div class="avatar examples-wrapper">
+            ${renderAvatarExamples()}
+        </div>
+      </div>
+      <div class="avatar-wrapper upload">
+        <div class="avatar update-type">
+            사진 올리기
+        </div>
+        <div class="avatar upload-wrapper">
+            <input type="file" id="avatar-upload" accept="image/*">
+        </div>
+      </div>
+    `,
+    );
+  };
+
   init();
+  render();
 }
