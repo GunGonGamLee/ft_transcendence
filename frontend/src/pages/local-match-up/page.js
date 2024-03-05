36fdeb847b30435a75da634d5b0606b0ddab6b72
donghyun <donghyun1998@naver.com>
2024-03-05 15:12:35 +0900

    feat: animation css 추가

--- frontend/src/pages/local-match-up/page.js
+++ frontend/src/pages/local-match-up/page.js
@@ -1,6 +1,7 @@
 import playerNameInput from "./playerNameInput.js";
 import { click } from "../../utils/clickEvent.js";
 import { navigate } from "../../utils/navigate.js";
+import { importCss } from "../../utils/importCss.js";
 export default function LocalMatchup($container, info = null) {
   // TODO: router 에서 info를 참조로 비교하므로 구조분해할당 후 넘겨야할듯
   info = {
@@ -27,7 +28,14 @@ export default function LocalMatchup($container, info = null) {
     click($container.querySelector(".start-btn"), () => {
       for (let i = 1; i <= 4; i++) {
         if (info.match[`player${i}`] === "") {
-          alert(`${i}P 이름을 입력해주세요`);
+          // alert(`${i}P 이름을 입력해주세요`);
+          const $input = $container.querySelector(`.player-name-input-${i}`);
+          $input.focus();
+          $input.classList.add("shake-animation");
+          // 애니메이션 보여준 후 제거
+          $input.addEventListener("animationend", () => {
+            $input.classList.remove("shake-animation");
+          });
           return;
         }
       }
@@ -36,6 +44,7 @@ export default function LocalMatchup($container, info = null) {
   };
 
   const render = () => {
+    importCss("../../../assets/css/localMatchUp.css");
     $container.innerHTML = `
       <div style="height: 100%; width: 100%; flex-direction: column; display: flex; justify-content: center; align-items: center; background-image: url('../../../assets/images/ingame_background4.png'); background-size: cover">
         ${playerNameInput(1)}
