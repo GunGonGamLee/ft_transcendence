f12c7b650ed519543b6265305a33c10a2dc49eda
yena <50291995+nyj001012@users.noreply.github.com>
2024-03-06 15:48:46 +0900

    feat: 아바타 업데이트 API 이식 (#397)
    * feat(avatar): 아바타 업데이트하는 API 이식
    
    * chore(avatar): 아바타 변경 페이지 jsdoc 추가

--- frontend/src/pages/avatar/page.js
+++ frontend/src/pages/avatar/page.js
@@ -7,6 +7,7 @@ import {
 import { importCss } from "../../utils/importCss.js";
 import { getUserMe } from "../../utils/userUtils.js";
 import { getCookie } from "../../utils/cookie.js";
+import { navigate } from "../../utils/navigate.js";
 
 export default function Avatar($container) {
   new Histories($container);
@@ -18,6 +19,10 @@ export default function Avatar($container) {
     importCss("../../../assets/css/avatar.css");
   };
 
+  /**
+   * 기본으로 제공되는 5개의 아바타 파일명을 이용하여 아바타 이미지를 렌더링하는 함수. 아바타 이미지를 클릭하면 해당 아바타로 변경된다.
+   * @returns { string } 아바타 이미지를 렌더링한 HTML 문자열
+   */
   const renderAvatarExamples = () => {
     let html = "";
     for (let avatar of AVATAR_FILE_NAME) {
@@ -28,6 +33,10 @@ export default function Avatar($container) {
     return html;
   };
 
+  /**
+   * 아바타 파일을 업로드하는 함수
+   * @param files { DataTransfer.files } 업로드할 파일
+   */
   const uploadAvatar = (files) => {
     const $avatarName = document.getElementById("avatar-name");
     $avatarName.value = files[0].name;
@@ -58,6 +67,33 @@ export default function Avatar($container) {
     });
   };
 
+  /**
+   * 사용자의 아바타를 업데이트하는 함수
+   * @param avatarName { string } 업데이트할 아바타 파일명으로, 확장자를 포함한다. 기본으로 제공되는 5개의 아바타 파일명 중 하나.
+   */
+  const updateAvatar = (avatarName) => {
+    getUserMe().then((response) => {
+      const { nickname } = response.data;
+      fetch(`${BACKEND}/users/${nickname}/avatar/?avatar=${avatarName}`, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${getCookie("jwt")}`,
+          "Content-type": "application/json",
+        },
+      }).then((response) => {
+        if (response.ok) {
+          const $userAvatar = document.getElementById("user-avatar");
+          $userAvatar.src = `${HISTORIES_IMAGE_PATH}/avatar/${avatarName}`;
+        } else {
+          navigate("error", { errorCode: response.status });
+        }
+      });
+    });
+  };
+
+  /**
+   * 아바타 변경 페이지를 렌더링하는 함수. 아바타 변경 페이지는 기본 아바타와 업로드 아바타로 구성되어 있다.
+   */
   const render = () => {
     this.$content.insertAdjacentHTML(
       "beforeend",
@@ -92,6 +128,14 @@ export default function Avatar($container) {
     $avatarUpload.addEventListener("change", (e) =>
       uploadAvatar(e.target.files),
     );
+
+    const $avatarExamples = document.getElementsByClassName("avatar examples");
+    for (let $avatar of $avatarExamples) {
+      $avatar.addEventListener("click", (e) => {
+        const avatarName = e.target.getAttribute("src").split("/").pop();
+        updateAvatar(avatarName);
+      });
+    }
   };
 
   init();
