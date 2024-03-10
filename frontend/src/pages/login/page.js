74ef7972ed7a50ed824e8f2e2ef2897b4c8e7ac3
chj <bluedog129@gmail.com>
2024-03-10 17:24:27 +0900

    refactor: 어긋나게 만들었던 불필요한 left속성 제거

--- frontend/src/pages/login/page.js
+++ frontend/src/pages/login/page.js
@@ -19,49 +19,49 @@ export default function Login($container) {
     }
     $container.innerHTML = `
         <div class="login-wrapper">
-        <div class="container">
-        <div class="arcade-machine">
-          <div class="shadow"></div>
-          <div class="top">
-            <h1>FT_TRANCENDENCE</h1>
-          </div>
-          <div class="screen-container">
-            <div class="shadow"></div>
-            <div class="screen">
-              <div class="screen-display">
+            <div class="container">
+            <div class="arcade-machine">
+              <div class="shadow"></div>
+              <div class="top">
+                <h1>FT_TRANCENDENCE</h1>
               </div>
-              <h2 id="google">Google Login</h2>
-              <h2 id="forty-two">42 Login</h2>
-              <div class="alien-container">
-                <div class="alien">
-                  <div class="ear ear-left"></div>
-                  <div class="ear ear-right"></div>
-                  <div class="head-top"></div>
-                  <div class="head">
-                    <div class="eye eye-left"></div>
-                    <div class="eye eye-right"></div>
+              <div class="screen-container">
+                <div class="shadow"></div>
+                <div class="screen">
+                  <div class="screen-display">
                   </div>
-                  <div class="body"></div>
-                  <div class="arm arm-left"></div>
-                  <div class="arm arm-right"></div>
+                  <h2 id="google">Google Login</h2>
+                  <h2 id="forty-two">42 Login</h2>
+                  <div class="alien-container">
+                    <div class="alien">
+                      <div class="ear ear-left"></div>
+                      <div class="ear ear-right"></div>
+                      <div class="head-top"></div>
+                      <div class="head">
+                        <div class="eye eye-left"></div>
+                        <div class="eye eye-right"></div>
+                      </div>
+                      <div class="body"></div>
+                      <div class="arm arm-left"></div>
+                      <div class="arm arm-right"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="joystick">
+                  <div class="stick"></div>
                 </div>
               </div>
+              <div class="board">
+                <div class="button button-a"></div>
+                <div class="button button-b"></div>
+                <div class="button button-c"></div>
+              </div>
+              <div class="bottom">
+                <div class="stripes"></div>
+              </div>
             </div>
-            <div class="joystick">
-              <div class="stick"></div>
-            </div>
-          </div>
-          <div class="board">
-            <div class="button button-a"></div>
-            <div class="button button-b"></div>
-            <div class="button button-c"></div>
-          </div>
-          <div class="bottom">
-            <div class="stripes"></div>
           </div>
         </div>
-      </div>
-      </div>
     `;
   };
 
