4be294bf5665449f31b3a672b5225fda284735a0
yena <50291995+nyj001012@users.noreply.github.com>
2024-03-12 18:11:11 +0900

    test(header): 메인 헤더 테스트 전마다 쿠키 세팅 및 메인 헤더 렌더링 성공 케이스 구조 정리

--- tests/header/mainHeader/mainHeader.spec.js
+++ tests/header/mainHeader/mainHeader.spec.js
@@ -6,16 +6,30 @@ test.use({
   javaScriptEnabled: true,
 });
 
-test("test", async ({ page, browser }) => {
-  const context = await browser.newContext();
+test.beforeEach(async ({ page, context, browser }) => {
   await context.addCookies([
     {
       name: "jwt",
       value: process.env.JWT_TOKEN,
       domain: "localhost",
       path: "/",
+      httpOnly: true,
     },
   ]);
   await page.goto("/game-mode");
-  await context.close();
+});
+
+test.describe("Main Header", () => {
+  test("should render main header", async ({ page }) => {
+    const userInfoButton = page.getByRole("button", {
+      name: "예나 아바타",
+    });
+    await userInfoButton.waitFor({ state: "visible" });
+    await expect(userInfoButton).toHaveText("예나");
+    // await page.getByText("사십 이 초-월").click();
+    // await page.getByRole("img", { name: "뒤로가기" }).click();
+    // await page.getByRole("img", { name: "친구 목록" }).click();
+    // await page.getByRole("img", { name: "친구 목록" }).click();
+    // await page.getByText("떠나기").click();
+  });
 });
