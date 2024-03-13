e807765d042f95ada0d758d7c860c0984a9b78bd
yena <50291995+nyj001012@users.noreply.github.com>
2024-03-13 15:52:45 +0900

    feat(utils): 시간에서 .을 제외하는 포맷팅 함수 추가

--- frontend/src/utils/dateUtils.js
+++ frontend/src/utils/dateUtils.js
@@ -13,3 +13,15 @@ export function formatDateWithTime(dateString) {
   const formattedTime = date.toTimeString().split(" ")[0];
   return formattedDate + " " + formattedTime;
 }
+
+/**
+ * 시간을 포맷팅하여 반환합니다. (HH:mm:ss)
+ * @param timeString {string} 시간 문자열
+ * @returns {string} 포맷팅된 시간 문자열
+ */
+export function formatTime(timeString) {
+  const [hour, minute, second] = timeString.split(":");
+  return (
+    hour.split(".")[0] + ":" + minute.split(".")[0] + ":" + second.split(".")[0]
+  );
+}
